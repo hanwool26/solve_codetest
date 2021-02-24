2a436ab5b78d2407cdada0edf5f117cc2d991067
Hanwool Lee <hanwool26@gmail.com>
2021-02-24 22:05:07 +0900

    Update target_number.py
    solution #1

--- Programmers/DFS_BFS/target_number.py
+++ Programmers/DFS_BFS/target_number.py
@@ -1,1 +1,34 @@
 # Target number solution
+
+def permute(n, r) -> int:
+    k = 1
+    j = 1
+    for _ in range(r):
+        k *= n
+        j *= r
+        n = n-1
+        r = r-1
+    return int(k/j)
+    
+
+def solve(numbers, target):
+    answer = 0
+    sum = 0
+    r=0
+    
+    for i in numbers:
+        sum += i
+    
+    while True:
+        sum -= numbers[0]
+        if sum == target:
+            break
+        elif sum == 0:
+            answer = -1
+        r += 1
+    
+    if answer != -1:
+        answer = permute(len(numbers), r)
+    
+    
+    return answer
