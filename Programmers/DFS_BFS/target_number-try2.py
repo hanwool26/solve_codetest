55161adb9bf0eb332e5c5c08f9da499d7fd00085
Hanwool Lee <hanwool26@gmail.com>
2021-02-26 15:33:48 +0900

    Update target_number-try2.py

--- Programmers/DFS_BFS/target_number-try2.py
+++ Programmers/DFS_BFS/target_number-try2.py
@@ -1,35 +1,38 @@
-numbers = [1,1,1,1,1]
-target = 3
 count = 0
-init_number = 0
-stack = [[]*1 for i in range(10)]
-def find_target(k, target, operator, len_numbers, depth):
+
+# For Debug
+#stack = [[] for i in range(10)]
+
+def find_target(k, target, operator, len_numbers, depth, numbers):
     global count    
     global init_number
     global stack
-   
-    print(f'value : {k}, depth : {depth}, count : {count}, operator : {operator}')
     
-    stack[depth].append(k)
+    # For debug
+    # print(f'value : {k}, depth : {depth}, count : {count}, operator : {operator}')
+    #stack[depth].append(k)
     
-    if depth == len_numbers:
+    if depth == len_numbers-1:
         if k == target:
             count = count + 1
-        return 
-         
-    find_target(k-init_number, target, '-', len_numbers, depth+1)
-    find_target(k+init_number, target, '+', len_numbers, depth+1)
+        return
+    elif depth+1 >= len_numbers:
+        return
+    
+    find_target(k-numbers[depth+1], target, '-', len_numbers, depth+1, numbers)
+    find_target(k+numbers[depth+1], target, '+', len_numbers, depth+1, numbers)
 
 def solution(numbers, target):
     answer = 0
     global init_number
 
     len_numbers = len(numbers)
-    init_number = numbers[0]
-    find_target(0, target, '-', len_numbers, 0)
+    print(numbers)
+    find_target(0, target, '-', len_numbers, -1, numbers)
     
+    '''
+    # For Debug
     for i in range(len_numbers+2):
         print(stack[i])
+    '''
     return count
-
-solution(numbers, target)
