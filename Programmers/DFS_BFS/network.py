baf9d49eb379929b75c601b256ee54ef13317a8c
Hanwool Lee <hanwool26@gmail.com>
2021-03-01 15:43:31 +0900

    Update network.py

--- Programmers/DFS_BFS/network.py
+++ Programmers/DFS_BFS/network.py
@@ -1,1 +1,45 @@
+count = 0
 
+def is_visited_changed(old, new) -> bool:
+    # print(f'old:{old}, new:{new}')
+    global count
+    if old == new:
+        return False
+    else:
+        count += 1
+        return True
+
+def dfs(comp, visited, node, computers):
+    global count
+    k = -1
+    if node not in visited: 
+        visited.append(node)
+        for neighbour in computers[node]:
+            k += 1
+            if k == node: # 자기자신은 ixi 이므로 continue
+                # print(f'k={k}, node={node}')
+                continue
+            if neighbour == 1:
+                dfs(computers[k], visited, k, computers)
+    else:
+        # print('node is already visited', node)
+        pass
+            
+
+def solution(n, computers):
+    answer = 0
+    global count
+    visited = []
+    old = []
+    for i in range(n):
+        dfs(computers[i], visited, answer, computers)
+        # print('visited :', visited)
+        if is_visited_changed(old, visited):
+            #count += 1
+            old = visited.copy()
+
+        answer += 1
+        
+    # print('visitied list :', visited)
+    # print('count :', count)
+    return count
