217934423953c9ac32b72ddf9c68947baa5aa7a8
Hanwool Lee <hanwool26@gmail.com>
2021-03-02 12:24:15 +0900

    Update network.py

--- Programmers/DFS_BFS/network.py
+++ Programmers/DFS_BFS/network.py
@@ -1,7 +1,6 @@
 count = 0
 
 def is_visited_changed(old, new) -> bool:
-    # print(f'old:{old}, new:{new}')
     global count
     if old == new:
         return False
@@ -14,26 +13,21 @@ def dfs(comp, visited, node, computers):
     k = -1
     if node not in visited: 
         visited.append(node)
-        for neighbour in computers[node]:
-            k += 1
+        for k, neighbour in enumerate(computers[node]):
             if k == node: # 자기자신은 ixi 이므로 continue
-                # print(f'k={k}, node={node}')
                 continue
             if neighbour == 1:
                 dfs(computers[k], visited, k, computers)
-    else:
-        pass
             
+
 def solution(n, computers):
     answer = 0
+    global count
     visited = []
     old = []
     for i in range(n):
-        dfs(computers[i], visited, answer, computers)
-        # print('visited :', visited)
+        dfs(computers[i], visited, i, computers)
         if is_visited_changed(old, visited):
-            #count += 1
-            old = visited.copy() # = 로 대입 시, 같은 id를 참조하게됨. copy 연산자 활용하여 list 복제해야함(swallow copy)
-        answer += 1
-   
+            old = visited.copy()
+            
     return count
