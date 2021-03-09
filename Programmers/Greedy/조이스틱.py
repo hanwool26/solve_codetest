75b4a0d4cc8ce772b8086a34e797d95d8fb89c83
Hanwool Lee <hanwool26@gmail.com>
2021-03-09 12:21:01 +0900

    Update 조이스틱.py

--- Programmers/Greedy/조이스틱.py
+++ Programmers/Greedy/조이스틱.py
@@ -1,27 +1,31 @@
 def solution(name):
     answer = 0
-    i = 0
     count = 0
-    center_char = 'N'
-
-    for i in name:
-        print('i', i, count)
-        if i < center_char:
+    default = 'A'*len(name)
+    list_default = []
+    trace_list = []
+    standard_char = int((ord('A')+ord('Z'))/ 2)
+    
+    for index, i in enumerate(name):
+        if name == default:
+            break
+        if ord(i) < standard_char:
             for _ in range(ord('A'), ord(i)):
                 count+=1
+                trace_list.append('up')                
         elif i == 'A':
             continue
         else:
             for _ in range(ord(i)-1, ord('Z')):
                 count+=1
+                trace_list.append('down')
+        count += 1
+        trace_list.append('right')
         
-        count+=1
-    
-    print('last', name[len(name)-2])
-    if name[len(name)-2] == 'A':
-        count-=1
-           
-    print('count', count-1)
-    
-    answer = count -1
+        list_default = list(default)
+        list_default[index] = i
+        default = ''.join(list_default)
+        
+    print('trace', trace_list)
+    answer = count - 1
     return answer
