ed9276f913dfa730f38228b28f53415b87cc0df8
Hanwool Lee <hanwool26@gmail.com>
2021-03-10 17:04:14 +0900

    Update 입국심사.py

--- Programmers/BinarySearch/입국심사.py
+++ Programmers/BinarySearch/입국심사.py
@@ -1,37 +1,35 @@
-def binary_search(start, end, target, times, data):
+def binary_search(start, end, target, times):
     count = 0
-    # print('start', start, 'end', end)
-    
     if start > end:
         return
     
     mid = int((start+end) // 2)
-    for i in range(len(data)):
-        count+= mid//times[i]
-    
-    # print('mid', mid, 'count', count, 'target', target)
-
+    for i in range(len(times)):
+        count+= mid//times[i]    
+    print('mid', mid, 'count', count, 'target', target)
     if count == target:
         for i in times:
-            # print('moudula', mid%i)
+            print('moudula', mid%i)
             if mid % i == 0:
                 return mid
     count+=1
-
     if count > target:
         end = mid - 1
     else:
         start = mid + 1        
-    return binary_search(start,end,target,times,data)    
+    return binary_search(start,end,target,times)    
 
 def solution(n, times):
     answer = 0
     data = times.copy()
     
-    for i in range(len(data)):
-        data[i] = data[i]*n
+    if n <= len(times):
+        answer = max(times)
+    else:
+        answer = binary_search(0, n*max(data), n, times)
     
-    answer = binary_search(0, max(data), n, times, data)
+    print(answer)
+    return answer
     
     #print(answer)
     return answer
