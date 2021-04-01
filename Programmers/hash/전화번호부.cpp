5f1afd66d14dd692c241d8fd86c3b5f167936bc9
Hanwool Lee <hanwool26@gmail.com>
2021-04-01 21:39:49 +0900

    Update 전화번호부.cpp

--- Programmers/hash/전화번호부.cpp
+++ Programmers/hash/전화번호부.cpp
@@ -1,29 +1,26 @@
 #include <string>
 #include <vector>
-#include <unordered_set>
 #include <iostream>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    vector<string> hash;
-    bool answer = true;    
-    int pos;
-    for(auto &i : phone_book){    
-        for(auto &j:hash){
-        //cout << "p : " << i << " ha" << j << endl;
-        if(i.length() > j.length()){
-            pos = i.find(j);            
-        } else {
-            pos = j.find(i);
-        }
-        if(pos != string::npos){
-            answer = false;
-            break;
-        }
-        }
-  
-        hash.push_back(i);
+    bool answer = true;
+    vector<string> v[10];
+    int k;
+    
+    for(auto &p:phone_book){
+    	int index = p[0]-48;
+    	for(auto &i:v[index]){
+    		k = min(i.length(), p.length());
+    		if(i.substr(0, k) == p.substr(0,k)){
+    			//cout << "i : " << i << " p : " << p;
+    			answer = false;
+    			break;
+    		}
+    	}
+    	if (answer == false) break;
+    	v[index].push_back(p);
     }
-    return answer;
+    return answer;	
 }
