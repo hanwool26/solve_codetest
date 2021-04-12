8b88ba5c2b65192cadefb70e85b67fd2e9c16db9
Hanwool Lee <hanwool26@gmail.com>
2021-04-12 16:03:05 +0900

    Update balance_weight.cpp

--- 대기업기출/balance_weight.cpp
+++ 대기업기출/balance_weight.cpp
@@ -6,23 +6,71 @@
 
 using namespace std;
 
-int check_possible(set<int> s, int min, int max){
+int adding_balance_weight(vector<int> dp, int max, int min, set<int> remain_set){
+	int max_count = 0;
+	int count;
+	vector<int> temp;	// temporary vector
+	int adding_bw;
+	
+	for(int i=1; i<=500; i++){
+		temp.assign(dp.begin(), dp.end());
+		temp.push_back(i);
+		
+		int temp_size = temp.size();
+		for(int j=0; j<temp_size; j++){
+			int dp_pop = temp[j];
+			int plus = i+dp_pop;
+			int minus = abs(i-dp_pop);
+			
+			if(plus <= max && plus >= min) temp.push_back(plus);
+			if(minus <= max && minus >= min) temp.push_back(minus);
+		}
+		
+		for(auto &i:temp){
+			if(remain_set.find(i)!=remain_set.end()){
+				count +=1;				
+			}
+		}
+		if(max_count < count){
+			max_count = count;
+			adding_bw = i;
+			// cout << "max adding bw : " << i << endl;
+		}
+		temp.clear();	
+		count = 0;		
+	}
+	return adding_bw;		
+}
+
+
+
+int check_possible(set<int> s, vector<int> v, int min, int max){
 	int count = 0;
 	vector<int> remain;
+	set<int> remain_set;
 	for(int i = min; i<=max; i++){
 		if(s.find(i) != s.end()) {
-			cout << i << " ";
+			// cout << i << " ";
 			continue;
 		}else{
 			remain.push_back(i);
 		}
 	}
-	cout << endl << "Remain" << endl;
-	for(auto &i:remain){
-		cout << i << " ";
+	
+	if(remain.size() == 0) return 0;
+	else{		
+		for(auto &i:remain){
+			remain_set.insert(i);
+		}
+		/*
+		cout << endl << "Remain" << endl;
+		for(auto &i:remain){
+			cout << i << " ";
+		}
+		*/
+		return adding_balance_weight(v, min, max, remain_set);
+		cout << endl;
 	}
-	cout << endl;
-	return count;	
 }
 
 int solution(vector<int> v, int min, int max){
@@ -44,25 +92,21 @@ int solution(vector<int> v, int min, int max){
 			if(minus <= max && minus >= min) dp.push_back(minus);
 		}
 	}	
-	/*
-	sort(dp.begin(), dp.end());
-	dp.erase(unique(dp.begin(), dp.end()), dp.end());
-	*/
+		
 	for(auto &i:dp){
 		s.insert(i);
 	}	
-	return check_possible(s, min, max);
+	return check_possible(s, dp, min, max);
 	
 }
 
 int main(void){
 	int answer = 0;
 	int min_weight = 1;
-	int max_weight = 10;
-	vector<int> balance_weight = {8,4,9};	
+	int max_weight = 15;
+	vector<int> balance_weight = {1,2,4,8};	
 	
 	answer = solution(balance_weight, min_weight, max_weight);
-	cout << endl;
 	cout << "answer : " << answer;
 	return 0;
 }	
