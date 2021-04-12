62ca8615a958867929bd356cbf6c4d79f7d4bed1
Hanwool Lee <hanwool26@gmail.com>
2021-04-12 22:54:46 +0900

    Update 가장큰정사각형찾기.cpp

--- Programmers/Practice/가장큰정사각형찾기.cpp
+++ Programmers/Practice/가장큰정사각형찾기.cpp
@@ -9,12 +9,29 @@ using namespace std;
 int solution(vector<vector<int>> board)
 {
 	int answer = 0;
+	int size_x = board.size();
+	int size_y = board[0].size();
+
+	int dir_x;
+	int dir_y;	
 	
-	for(int i=0; i<board.size(); i++){
-		for(int j=0; j<board[i].size(); j++){
-			cout << board[i][j] << " ";
+	for(int i=0; i<size_x; i++){
+		for(int j=0; j<size_y; j++){
+			if(board[i][j] == 1){
+				cout << i << " " << j << endl;
+				dir_x = i;
+				dir_y = j;
+				
+				while(board[dir_x][j]!=0 && dir_x < size_x){
+					dir_x++;
+				}				
+				
+				while(board[i][dir_y]!=0 && dir_y < size_y){
+					dir_y++;					
+				}
+				cout << i << ", " << j << " / " << dir_x << ", " << dir_y << endl;
+			}
 		}
-		cout << endl;
 	}
     return answer;
 }
