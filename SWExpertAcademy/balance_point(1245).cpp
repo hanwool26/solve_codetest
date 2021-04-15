ef30a3a68f2cb7ad6add2854f8bdf155dd9c9403
Hanwool Lee <hanwool26@gmail.com>
2021-04-15 22:22:09 +0900

    Update balance_point(1245).cpp

--- SWExpertAcademy/balance_point(1245).cpp
+++ SWExpertAcademy/balance_point(1245).cpp
@@ -1,80 +1,64 @@
 // No. 1245
-
-#include <iostream>
-#include <cstdio>
-#include <string>
-#include <sstream>
-#include <cmath>
-#include <vector>
+#include<iostream>
 
 using namespace std;
 
-double binarySearch( long double x1,  long double x2, int m1, int m2, int y){
-	//cout << x1 << " " << x2 << " " << m1 << " " << m2 << endl;
-	if(x1>x2) return 0;
+void solution(int N, int x[], int w[]){
+	double result, mid, left, right;
 	
-	long double d = (x2-x1)/2;
-	long double x1_f = m1/pow(d,2);
-	long double x2_f = m2/pow((y-d),2);
-	cout << fixed;
-	cout.precision(11);
-	cout << "d : " << d << " " <<  (x1_f - x2_f) << endl;
-	cout.unsetf(ios::fixed);
-	
-	if(abs(x1_f - x2_f)<=0.1){
-		return d;		
-	} else if ( x1_f - x2_f < 0 && abs(x1_f - x2_f) > 0.1){
-		x2 = x1 + d - 0.0000000001;
-		cout << "x2 : " << x2 << endl;
-		return binarySearch(x1, x2, m1, m2, y);
-	} else if ( x1_f - x2_f > 0 && abs(x1_f - x2_f) > 0.1){
-		x1 = x1 + d + 0.0000000001;
-		cout << "x1 : " << x1 << endl;
-		return binarySearch(x1, x2, m1, m2, y);
-	}	
+	for(int idx=0; idx<N-1; idx++){
+		left = x[idx];
+		right = x[idx+1];		
+		
+		for(int k=0; k<100; k++){
+			mid = (left+right) / 2.0;
+			result = 0;
+			for(int i=0; i<=idx; i++){
+				result += w[i] / ((mid-x[i])*(mid-x[i]));
+			}
+			for(int j=idx+1; j<N; j++){
+				result -= w[j] / ((mid-x[j])*(mid-x[j]));
+			}
+			
+			if (result > 0){
+				left = mid;
+			}else{
+				right = mid;
+			}
+		}
+		cout << fixed;
+		cout.precision(10);
+		cout << mid << " ";
+	}		
 }
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-	string input;
-	vector<int> v;
-	char buffer[256];
-	istringstream ss;
 	
-	//	ios::sync_with_stdio(false);
-	// cin.tie(0); cout.tie(0);
-	
-	freopen("input.txt", "r", stdin);
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	freopen("balance_point_input.txt", "r", stdin);
 	cin>>T;
 	/*
 	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 	*/
+	cout << fixed;
+	cout.precision(10);
 	for(test_case = 1; test_case <= T; ++test_case)
 	{
 		int N;
-		int i = 0;
-		
-		double balance;
-		
 		cin >> N;
-		getchar();
-		cout << N << endl;
-		cin.getline(buffer, 256);
-		istringstream ss(buffer);
-		while(getline(ss, input, ' ')){
-			v.push_back(stoi(input));
-			i++;
-		}		
-		for(int i=0; i<N-1; i++){
-			for(int j=N; j<2*N-1; j++){
-				balance = binarySearch((v[i]), (v[i+1]), v[j], v[j+1], v[i+1]-v[i]);
-				cout << balance << endl;
-			}
-		}
-		v.clear();
-
+		int x[N];
+		int w[N];
+		for(int i=0; i<N; i++) cin >> x[i];
+		for(int i=0; i<N; i++) cin >> w[i];
+		
+		cout << "#" << test_case << " ";
+		solution(N,x,w);
+		cout << endl;
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
