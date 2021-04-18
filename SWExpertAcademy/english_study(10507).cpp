a2150bea24bca9e247aec4c246aa044b63b27af7
Hanwool Lee <hanwool26@gmail.com>
2021-04-18 10:30:00 +0900

    Update english_study(10507).cpp

--- SWExpertAcademy/english_study(10507).cpp
+++ SWExpertAcademy/english_study(10507).cpp
@@ -1,4 +1,3 @@
-// #10507
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -7,13 +6,16 @@
 using namespace std;
 
 int solution(vector<int> v, int n, int p){
-    int min = v.front();
-    int max = v.back();
+    int min_value = v.front();
+    int max_value = v.back();
     queue<pair<int, int>> q;
     int max_day = 0;
     int idx = 0;
+    cout << min_value << " " << max_value << " " << p << endl;
 
-    for(int i = min; i<max; i++){
+    cout << "max : " << max_value+p << endl;
+    for(int i=0; i<=max_value+p; i++){     
+        cout << i << endl;   
         if(i==v[idx]){
             q.push(make_pair(i, 1));
             idx++;
@@ -21,15 +23,15 @@ int solution(vector<int> v, int n, int p){
             q.push(make_pair(i, 0));
             p--;
         }else if (p==0){
-            if(!q.empty()){
-                while(q.front().second){
+             if(max_day < q.size()) max_day = q.size();
+                while(!q.empty()){
+                    if(!q.front().second) break;
                     q.pop();
                 }
                 q.pop();
                 q.push(make_pair(i, 0));
-            }
-        } 
-        if(max_day < q.size()) max_day = q.size();
+        }
+        if(max_day < q.size()) max_day = q.size();        
     }
     return max_day;
 }
@@ -53,6 +55,7 @@ int main(int argc, char** argv)
 
         cin >> n;
         cin >> p;
+        cout << n << " " << p << endl;
         for(int i=0; i<n; i++){
             cin >> day;
             v.push_back(day);            
@@ -60,7 +63,6 @@ int main(int argc, char** argv)
         cout << "#" << test_case << " " << solution(v, n, p) << endl;
 	}
     end = clock();
-
     cout << "duration : " << (double)(end-start) / CLOCKS_PER_SEC << endl;
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
