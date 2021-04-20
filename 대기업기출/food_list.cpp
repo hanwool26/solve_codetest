aae04292c3afd03b055a3e7a3b3c2fe40f83df61
Hanwool Lee <hanwool26@gmail.com>
2021-04-20 18:36:08 +0900

    Update food_list.cpp

--- 대기업기출/food_list.cpp
+++ 대기업기출/food_list.cpp
@@ -2,36 +2,56 @@
 #include <vector>
 using namespace std;
 
+typedef struct food{
+	int food_idx;
+	int carbo; 
+	int protein;
+	int fat;
+}Food;
+
 vector<int> food_list[3];
-void DFS(vector<vector<int>> food, vector<int> nutri_v[], vector<bool>& select, int idx, int cnt, int num) {
-	if (cnt == num) {
-		int f = 0;
-		int p = 0;
-		int s = 0;
-		for (int i = 0; i < food.size(); i++) {
-			if (select[i] == true) {
-				f+=food[i][0];
-				p+=food[i][1];
-				s+=food[i][2];				
-			}
+
+void print(Food food[], Food target[], vector<bool> &select, int food_size){
+	int cnt = 0;
+
+	for(int i=0; i<3; i++){
+		if(food_list[i].size() > 0){
+			cnt++;
 		}
-		for (int i = 0; i < 3; i++) {
-			if (nutri_v[i][0] == f && nutri_v[i][1] == p && nutri_v[i][2] == s) {
-				if (food_list[i].size() > 0) continue;
-				for (int j = 0; j < food.size(); j++) {						
-					if (select[j] == true) {
-						//cout << i + 1 << " ";
-						food_list[i].push_back((j + 1));
-					}					
-				}
-			}
+	}
+	if (cnt==3) return;	
+	int c=0;
+	int p=0;
+	int f=0;
+	vector<int> temp;
+	for(int i=0; i<food_size; i++){
+		if(select[i] == true){
+			cout << food[i].food_idx << " ";
+			c+=food[i].carbo;
+			p+=food[i].protein;
+			f+=food[i].fat;
+			temp.push_back(food[i].food_idx);						
 		}
+	}
+	cout << endl;
+	for(int j=0; j<3; j++){
+		if(food_list[j].size() > 0) continue;
+		if(target[j].carbo == c && target[j].protein == p && target[j].fat == f){
+			cout << "found" << endl;
+			food_list[j].assign(temp.begin(), temp.end());
+		}
+	}
+}
+
+void DFS(Food food[], Food target[], vector<bool> &select, int food_size, int cnt, int num, int idx){
+	if(cnt == num) {
+		print(food, target, select, food_size);
 		return;
 	}
-	for (int i = idx; i < food.size(); i++) {
-		if (select[i] == true) continue;
+	for(int i=idx; i<food_size; i++){
+		if(select[i] == true) continue;
 		select[i] = true;
-		DFS(food, nutri_v, select, idx, cnt + 1, num);
+		DFS(food, target, select, food_size, cnt+1, num, i);
 		select[i] = false;
 	}
 }
@@ -44,43 +64,40 @@ int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
 
-	freopen("sample_input.txt", "r", stdin);
+	freopen("nutri_input.txt", "r", stdin);
 	cin >> N;
 
 	for (testcase = 0; testcase < N; testcase++) {
-		int num_food; cin >> num_food;
-		vector<vector<int>> food;
-		vector<int> nutri_v[3];
-		vector<bool> select(num_food, false);
+		int food_num; cin >> food_num;
+		Food food[food_num];
+		Food target[3];
+		vector<bool> select(food_num, false);
+		for(int i=0; i<food_num; i++){
+			int c,p,f;
+			cin >> c; cin >> p; cin >> f;
+			food[i] = {i+1, c, p, f};
+		}
 		
-		for (int i = 0; i < num_food; i++) {
-			vector<int> temp;
-			food.push_back(temp);
-			for (int j = 0; j < 3; j++) {
-				cin >> input;
-				food[i].push_back(input);
-			}
+		for(int i=0; i<3; i++){
+			int c,p,f;
+			cin >> c; cin >>p; cin >> f;
+			target[i] = {i+1, c, p, f};
 		}
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				cin >> input;
-				nutri_v[i].push_back(input);
-			}
+		cout << "Hi" << endl;	
+		for(int i=1; i<food_num; i++){
+			DFS(food, target, select, food_num, 0, i, 0);	
 		}
-		for (int i = 1; i < num_food; i++) {
-			DFS(food, nutri_v, select, 0, 0, i);
-		}
-
-		for (int i = 0; i < 3; i++) {
+		
+		for(int i=0; i<3; i++){
 			cout << "#" << i << " ";
-			if (food_list[i].size() == 0) {
-				cout << "0";
-			}
-			for (auto& v : food_list[i]) {
-				cout << v << " ";
+			if (food_list[i].size() == 0) cout << "0" << endl;
+			else{
+				for(auto &v:food_list[i]){
+					cout << v << " ";
+				}
 			}
 			cout << endl;
 		}
-	}
+	}	
 	return 0;
 }
