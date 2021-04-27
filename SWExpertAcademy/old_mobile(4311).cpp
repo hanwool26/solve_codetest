cbdd957a68fc22b3a0e8cacab38e05554b0c1741
Hanwool Lee <hanwool26@gmail.com>
2021-04-27 17:38:56 +0900

    Update old_mobile(4311).cpp

--- SWExpertAcademy/old_mobile(4311).cpp
+++ SWExpertAcademy/old_mobile(4311).cpp
@@ -1,38 +1,16 @@
 #include <iostream>
 //#include <vector>
-#include <string>
+//#include <string>
 
 using namespace std;
 
 int dp[1000];
 int origin[1000];
 bool op[5];
-int num[10];
+bool num[10];
 bool visit[10];
 int answer;
 int N, O, M, W;
-
-typedef class vector{
-	public:
-		int arr[10];
-		int index;
-		vector(){
-			index = 0;
-			//size = 0;
-			for(int i=0; i<10; i++){
-				arr[i] = 0;
-			}
-		}		
-		void push_back(int num){
-			arr[index++] = num;
-		}		
-		void pop_back(){
-			arr[index--] = 0;
-		}		
-		int get_size(){
-			return index;
-		}
-}vector;
 
 void init_testcase(){
 	for(int i=0; i<1000; i++){
@@ -41,34 +19,33 @@ void init_testcase(){
 	for(int i=1; i<5; i++){
 		op[i] = false;
 	}
-	for(int i=0; i<9; i++){
-		num[i] =0;
+	for(int i=0; i<10; i++){
+		num[i] =false;
 		visit[i] = false;
 	}	
 	answer= 21;
 }
 
-void solution(int num, int cnt, bool flag){
-	if (cnt + flag > M || cnt+flag > answer) return;
-	if (num == W) {	
-		
-		cout << dp[16] << " " << dp[62] << " " << dp[992] << " " << dp[W] << endl;
+void solution(int l, int cnt, bool flag){
+	if (cnt+flag > M || cnt+flag > answer) return;
+	if (l == W) {	
 		answer = cnt + flag;
 		return;
-	}
-	
+	}	
 	for(int i=0; i<1000; i++){		
 		if(!dp[i] || !origin[i]) continue;		
+		//cout << i << endl;
 		for(int j=1; j<5; j++) {
 			if(!op[j])continue;
 			if(i==0 && j==4) continue;
 			
 			int temp;
-			if(j==1) temp = num+i;
-			else if(j==2) temp = num-i;
-			else if(j==3) temp = num*i;
-			else if(j==4) temp = num/i;
+			if(j==1) temp = l+i;
+			else if(j==2) temp = l-i;
+			else if(j==3) temp = l*i;
+			else if(j==4) temp = l/i;
 			
+
 			if(temp < 0 || temp > 999) continue;
 			if(dp[temp] == 0 || dp[temp] > cnt+dp[i]+1) {
 				
@@ -79,6 +56,7 @@ void solution(int num, int cnt, bool flag){
 	}	
 }
 /* 주어진 숫자로 연산자 없이 만들 수 있는 수 */ 
+/*
 void make_number(vector &temp, bool visit_[], int cnt, int N, int target){
 	if(cnt == target){
 		int int_temp;
@@ -99,7 +77,26 @@ void make_number(vector &temp, bool visit_[], int cnt, int N, int target){
 		temp.pop_back();
 		visit_[i] = false;
 	}	
-}
+}*/
+
+void make_origin(int dial, int cnt) {
+	if (cnt==0){
+		if(num[0]) {
+			origin[0] = dp[0] = 1;
+		}
+		for(int i=1; i<10; i++){
+			if (num[i]) make_origin(i, 1);
+		}
+		return;
+	}
+	if(!dp[dial]) origin[dial]=dp[dial]=cnt;
+	else if (dp[dial] > cnt) origin[dial]=dp[dial]=cnt;
+	
+	if(dial > 99) return;
+	for(int i=0; i<10; i++){
+		if(num[i]) make_origin(dial*10+i, cnt+1);
+	}
+}	
 
 int main(){	
 	ios::sync_with_stdio(false);
@@ -110,30 +107,24 @@ int main(){
 	
 	cin >> T;
 	
-	for(int testcase=0; testcase < T; testcase++){
-		
+	for(int testcase=0; testcase < T; testcase++){		
 		cin >> N >> O >> M;
-		vector v_temp;
-		
 		init_testcase();
 		for(int i=0; i<N; i++){
 			cin >> temp;
-			num[i]=temp;
-			dp[temp]=origin[temp]=1;
+			num[temp]=true;
 		}
 		for(int i=0; i<O; i++){
 			cin >> temp;
 			op[temp]=true;
 		}
 		cin >> W;
-		for(int i=2; i<=3; i++) make_number(v_temp, visit, 0, N, i);
+		make_origin(0,0);
 		if(dp[W]>0){
 			answer = dp[W];
 		}else{
 			for(int i=0; i<1000; i++){
-				//cout << i << " " << dp[i] << endl;
 				if(dp[i] && origin[i]) {
-					//cout << i << " ";					
 					solution(i, dp[i], 0);	
 				}
 			}
