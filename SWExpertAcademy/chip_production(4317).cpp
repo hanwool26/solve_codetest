a26e2474715c5684bf6bd73452b88449d7572bc4
Hanwool Lee <hanwool26@gmail.com>
2021-04-29 19:41:43 +0900

    Update chip_production(4317).cpp

--- SWExpertAcademy/chip_production(4317).cpp
+++ SWExpertAcademy/chip_production(4317).cpp
@@ -8,9 +8,6 @@ using namespace std;
 vector<vector<int>> dp;
 int dp_idx;
 
-
-
-
 void init_case(){
 	for(int i=0; i<dp.size(); i++){
 		dp[i].clear();
@@ -139,5 +136,98 @@ int main(){
 		
 	}
 		
+	return 0;
+}
+
+# ====================================================================================
+# Using DFS Algorithm with Backtracking for finding out solution and meeting time limit
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int arr[10][25];
+int answer;
+int N, M;
+
+void init_case(){
+	for(int i=0; i<10; i++){
+		for(int j=0; j<25; j++){
+			arr[i][j] = 0;
+		}
+	}	
+	answer = 0;
+	N=0;
+	M=0;
+}
+bool isempty(int x, int y){
+	if(x+1 == N || y+1 == M) return false;
+	if(arr[x][y]==0 && arr[x+1][y]==0 && arr[x][y+1]==0 && arr[x+1][y+1] ==0){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+void print_arr(){
+	for(int i=0; i<N; i++){
+		for(int j=0; j<M; j++){
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << "-------------" << endl;
+}
+
+void solution(int x, int y, int cnt)
+{	
+	
+	if(x > N-1){
+		x = 0;
+		y = y+1;
+	}
+	if(y == M-1) {
+		if(answer < cnt) {
+			answer = cnt;		
+		}
+		return;
+	}
+	# Need to back traking technique in order to meet time limit. 
+	
+	if(isempty(x,y)){
+		arr[x][y] = arr[x+1][y] = arr[x][y+1] = arr[x+1][y+1] = 1;
+		solution(x+2, y, cnt+1);
+		arr[x][y] = arr[x+1][y] = arr[x][y+1] = arr[x+1][y+1] = 0;
+	}
+	solution(x+1, y, cnt);		
+}
+
+
+int main(){
+	int T;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	freopen("chip_production_input.txt", "r", stdin);
+	cin >> T;
+	
+	for(int tc=0; tc<T; tc++){
+		int input;		
+		init_case();
+				
+		cin >> N >> M;			
+		
+		for(int i=0; i<N; i++){
+			for(int j=0; j<M; j++){
+				cin >> input;
+				arr[i][j] = input;
+			}
+		}
+		solution(0,0,0);
+		
+		cout << "#" << tc+1 << " " << answer << endl;
+	}
 	return 0;
 }
