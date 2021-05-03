cb379e1203e100095053616ec877dccacf0e93ba
Hanwool Lee <hanwool26@gmail.com>
2021-05-03 21:42:25 +0900

    Update packet_handler(4322).cpp

--- SWExpertAcademy/packet_handler(4322).cpp
+++ SWExpertAcademy/packet_handler(4322).cpp
@@ -1,103 +1,76 @@
 #include <iostream>
-
+ 
 using namespace std;
-
-int packet[1000][2];
-int end_time;
-int answer;
-bool is_ok;
-int N;
-
+ 
 int cpu_endtime[5];
-
-void print_packet()
+int packet[1000][2];
+int answer;
+int N;
+int tc;
+ 
+unsigned char dp[1001][11][11][11][11][11];
+ 
+bool solution(int idx, int num_cpu)
 {
-	for (int i = 0; i<N; i++){
-		cout << packet[i][0] << " ";
-	}
-	cout << endl;
-	for (int i = 0; i<N; i++){
-		cout << packet[i][1] << " ";
-	}
-	cout << endl;
+    if (idx == N) {
+        return true;
+    }
+ 
+    int wq[5] = { 0, }; 
+    int start_time = packet[idx][0];
+    int handler_time = packet[idx][1];
+ 
+    for (int i = 0; i < num_cpu; i++) {
+        if (cpu_endtime[i] < start_time)
+            wq[i] = 0;
+        else
+            wq[i] = cpu_endtime[i] - start_time;
+    }
+ 
+    if (dp[idx][wq[0]][wq[1]][wq[2]][wq[3]][wq[4]] == tc*5 + num_cpu) return false;
+    dp[idx][wq[0]][wq[1]][wq[2]][wq[3]][wq[4]] = tc*5 + num_cpu;
+ 
+    for (int i = 0; i < num_cpu; i++) {
+        int end_time = cpu_endtime[i];
+        if (cpu_endtime[i] - start_time + handler_time > 10) continue;
+        if (wq[i] == 0)
+            cpu_endtime[i] = start_time + handler_time;
+        else
+            cpu_endtime[i] = cpu_endtime[i] + handler_time;
+        if (solution(idx + 1, num_cpu)) {
+            cpu_endtime[i] = end_time;
+            return true;
+        }
+        cpu_endtime[i] = end_time;
+    }
+    return false;
 }
-
-void init_case(){
-	for (int i = 0; i<1000; i++){
-		packet[i][0] = packet[i][1] = 0;
-	}
-	answer = -1;
-	end_time = 0;
-
-	is_ok = false;
-}
-
-void solution(int idx, int num_cpu, int cpu_idx)
-{
-	//cout << idx << endl;
-	if (idx == N) {
-		is_ok = true;
-		return;
-	}
-	int wait_time = 0;
-	int end_time = cpu_endtime[cpu_idx];
-
-	int start_time = packet[idx][0];
-	int handler_time = packet[idx][1];
-	if (cpu_endtime[cpu_idx] > start_time){
-		wait_time = cpu_endtime[cpu_idx] - start_time;
-	}
-	start_time = start_time + wait_time;
-	//cout << "cpu : " << cpu << " total time per packet : " \
-			<< "[" << idx << "] " << wait_time << " " << handler_time << " N : " << N << endl;
-
-	if (wait_time + handler_time > 10) {
-		return;
-	}
-	for (int i = 0; i<num_cpu; i++){
-		cpu_endtime[cpu_idx] = start_time + handler_time;
-		solution(idx + 1, num_cpu, i);
-		cpu_endtime[cpu_idx] = end_time;
-	}
-}
-
+ 
 int main()
 {
-	int T;
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-
-	freopen("packet_handler_input.txt", "r", stdin);
-	cin >> T;
-
-	for (int tc = 0; tc<T; tc++){
-		init_case();
-		int time, length;
-		cin >> N;
-		for (int i = 0; i<N; i++){
-			cin >> time >> length;
-			packet[i][0] = time;
-			packet[i][1] = length;
-		}
-		end_time = packet[0][0] + packet[0][1];
-
-		for (int i = 0; i < 5; i++){
-			cpu_endtime[i] = 0;
-		}
-
-		//print_packet();
-		for (int cpu = 1; cpu <= 5; cpu++){
-			solution(0, cpu, 0);
-
-			if (is_ok == true) {
-				answer = cpu;
-				break;
-			}
-			for (int i = 0; i < 5; i++){
-				cpu_endtime[i] = 0;
-			}
-		}
-		cout << "#" << tc + 1 << " " << answer << endl;
-	}
-	return 0;
+    int T;
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+ 
+    freopen("packet_handler_input.txt", "r", stdin);
+    cin >> T;
+ 
+    for (tc = 0; tc < T; tc++) {
+        answer = -1;
+        int time, length;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            cin >> time >> length;
+            packet[i][0] = time;
+            packet[i][1] = length;
+        }
+        for (int cpu = 1; cpu <= 5; cpu++) {
+            if (solution(0, cpu)) {
+                answer = cpu;
+                break;
+            }
+        }
+        cout << "#" << tc + 1 << " " << answer << endl;
+    }
+    return 0;
 }
