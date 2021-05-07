9ad4f32ba31db9352198079379e3b65841f77b0a
Hanwool Lee <hanwool26@gmail.com>
2021-05-07 15:30:43 +0900

    Update sos(4335).cpp

--- SWExpertAcademy/sos(4335).cpp
+++ SWExpertAcademy/sos(4335).cpp
@@ -1,78 +1,72 @@
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 
-typedef struct Box{
-	int w;
-	int h;
-	int l;
-}Box;
 int box[20][3];
-int answer = 0;
-int T, N, w, h, l;
+int dp[20][1 << 20 - 1][3];
+int answer;
+int N, w, l, h;
 
-void print_box(){
-	for (int i = 0; i < N; i++){
-		cout << box[i][0] << " " << box[i][1] << " " << box[i][2] << endl;
-	}
-}
+int DFS(int w, int d, int idx, int side, int visit){
 
-int is_below(int idx, int cp_idx){ // cp_idx : compared_idx
-	int dp[3] = { 0, };
-	int cnt = 0;
-	int remain = 0;
-	for (int i = 0; i < 3; i++){
-		for (int j = 0; j < 3; j++){
-			if (box[idx][i] <= box[cp_idx][j] && dp[j] == 0){
-				dp[j] = box[cp_idx][j];
-				cnt++;
-			}
+    int sum = 0;
 
-		}
-	}
-	if (cnt == 2){
-		for (int i = 0; i < 3; i++){
-			if (box[cp_idx][i] != dp[0] && box[cp_idx][i] != dp[1])
-				remain = box[cp_idx][i];
-		}
-	}
-	if (cnt == 3){
-		remain = *max_element(dp, dp + 3);
-	}
-	cout << "remain : " << remain << endl;
-	return remain;
-}
+    int& height = dp[idx][visit][side];
+    
+    if (height != -1) {
+        return height;
+    }    
 
-void DFS(int idx, int cnt, int sum){
-	if (cnt == N) return;
-	if (sum > answer) sum = answer; 
+    for (int i = 0; i < N; i++) {
+        if ((visit & (1 << i))!=0) continue;        
 
-	for (int i = 0; i < N; i++){
-		if (is_below(idx, i) != 0) continue;
-		else {
-		}
-		}
-	}
+        if ((w >= box[i][1] && d >= box[i][2]) || (w >= box[i][2] && d >= box[i][1])) {
+            sum = box[i][0] + DFS(box[i][1], box[i][2], i, 0, visit | (1<<i));
+            if (height < sum) {
+                height = sum;
+            }
+        }
+        if ((w >= box[i][0] && d >= box[i][2]) || (w >= box[i][2] && d >= box[i][0])) {
+            sum = box[i][1] + DFS(box[i][0], box[i][2], i, 1, visit | (1<<i));
+            if (height < sum) {
+                height = sum;
+            }
+        }
+        if ((w >= box[i][0] && d >= box[i][1]) || (w >= box[i][1] && d >= box[i][0])) {
+            sum = box[i][2] + DFS(box[i][0], box[i][1], i, 2, visit | (1<<i));
+            if (height < sum) {
+                height = sum;
+            }
+        }
+    }
+    if (sum == 0) {
+        height = sum; 
+    }
+    return height; 
 }
 
 int main()
-{	
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
+{
+    int T;
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
 
-	freopen("sos_input.txt", "r", stdin);
-	cin >> T;
+    freopen("sos_input.txt", "r", stdin);
 
-	for (int tc = 0; tc < 2; tc++){
-		cin >> N;
-		for (int i = 0; i < N; i++){
-			cin >> w >> h >> l;
-			box[i][0] = w;
-			box[i][1] = h;
-			box[i][2] = l;
-		}
-		print_box();
-		is_below(0, 1);
-		cout << "#" << tc+1 << " " << answer << endl;
-	}
+    cin >> T;
+
+    for (int tc = 0; tc < T; tc++) {        
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < (1 << N)-1; j++) {
+                dp[i][j][0] = dp[i][j][1] = dp[i][j][2] = -1;
+            }
+            cin >> w >> l >> h;
+            box[i][0] = w;
+            box[i][1] = l;
+            box[i][2] = h;
+        }
+        cout << "#" << tc+1 << " " << DFS(10001,10001,0,0,0) << endl;
+    }
+    return 0;
 }
