11fe0254444925d25cf2ad538868d6e10acfc348
Hanwool Lee <hanwool26@gmail.com>
2021-05-14 09:16:44 +0900

    Update pipe_connection(4340).cpp

--- SWExpertAcademy/pipe_connection(4340).cpp
+++ SWExpertAcademy/pipe_connection(4340).cpp
@@ -6,6 +6,15 @@ int visit[50][50];
 int N;
 int answer = 0;
 
+void init_case(){
+	for (int i = 0; i < 50; i++){
+		for (int j = 0; j < 50; j++){
+			map[i][j] = visit[i][j] = 0;
+		}
+	}
+	answer = N*N;
+}
+
 typedef struct _pair{
 	int x;
 	int y;
@@ -21,24 +30,84 @@ Pair pipe_dir[7][2] = {
 	{ {-1, 0 }, { 0, 1 } },	// 6
 };
 
-void DFS(int x, int y, int pipe, int cnt){
+enum direction{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+};
+
+void DFS(int x, int y, int dir, int cnt){	
+	if (x < 0 || x > N-1 || y < 0 || y > N-1) return;
+	int pipe = map[x][y];
+	if (pipe == 0) return;
+
+	printf("(%d,%d) pipe : %d\n", x, y, pipe);
 	if (x == N - 1 && y == N - 1 && (pipe == 1 || pipe == 6)){
 		if (answer > cnt){
 			answer = cnt;
+			cout << answer << endl;
 		}
 		return;
 	}
-	if (visit[x][y] == 1)return;
-	visit[x][y] = 1;
-	for (int i = 1; i < 7; i++){
-		for (int j = 0; j < 2; j++){
-			int next_x = x + pipe_dir[i][j].x;
-			int next_y = y + pipe_dir[i][j].y;
-			if (map[next_x][next_y] == 0) continue;
-			for (int k = 0; k < 2; k++){
-				if (pipe_dir[i][j].x+pipe_dir)
-			}
+	if (visit[x][y] == 1) return;
+	//printf("(%d,%d) pipe : %d\n", x, y, pipe);
+	int next_x, next_y;
+
+	if (pipe <= 2){
+		if (dir == UP){
+			next_x = x-1;
+			next_y = y;
+			DFS(next_x, next_y, UP, cnt + 1);
 		}
+		if (dir == DOWN){
+			next_x = x + 1;
+			next_y = y;
+			DFS(next_x, next_y, DOWN, cnt + 1);
+		}
+		if (dir == LEFT){
+			next_x = x;
+			next_y = y-1;
+			DFS(next_x, next_y, LEFT , cnt + 1);
+		}
+		if (dir == RIGHT){
+			next_x = x ;
+			next_y = y+1;
+			DFS(next_x, next_y, RIGHT, cnt + 1);
+		}
+	}
+	else{
+		if (dir == UP){
+			next_x = x;
+			next_y = y + 1;
+			DFS(next_x, next_y, RIGHT, cnt + 1);
+			next_y = y - 1;
+			DFS(next_x, next_y, LEFT, cnt + 1);
+		}
+		if (dir == DOWN){
+			next_x = x;
+			next_y = y + 1;
+			DFS(next_x, next_y, RIGHT, cnt + 1);
+			next_x = x;
+			next_y = y - 1;
+			DFS(next_x, next_y, LEFT, cnt + 1);
+		}
+		if (dir == LEFT){
+			next_x = x + 1;
+			next_y = y;
+			DFS(next_x, next_y, DOWN, cnt + 1);
+			next_x = x - 1;
+			next_y = y;
+			DFS(next_x, next_y, UP, cnt + 1);
+		}
+		if (dir == RIGHT){
+			next_x = x + 1;
+			next_y = y;
+			DFS(next_x, next_y, DOWN, cnt + 1);
+			next_x = x - 1;
+			next_y = y;
+			DFS(next_x, next_y, UP, cnt + 1);
+		}		
 	}
 	visit[x][y] = 0;
 }
@@ -58,8 +127,7 @@ int main(){
 				cin >> map[i][j];
 			}
 		}
-		answer = N*N;
-
+		DFS(0, 0, RIGHT, 1);
 		cout << "#" << tc + 1 << " " << answer << endl;
 	}
 }
