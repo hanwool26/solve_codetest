950cb051e8a3d3195d9763d6cd1d4f54653dcd39
Hanwool Lee <hanwool26@gmail.com>
2021-05-18 11:20:47 +0900

    Update sos(4335).cpp

--- SWExpertAcademy/sos(4335).cpp
+++ SWExpertAcademy/sos(4335).cpp
@@ -1,72 +1,60 @@
 #include <iostream>
-
 using namespace std;
 
-int box[20][3];
-int dp[20][1 << 20 - 1][3];
-int answer;
-int N, w, l, h;
+int num_block;
+int blocks[20][3];
+int dp[20][(1 << 20) - 1][3];
 
-int DFS(int w, int d, int idx, int side, int visit){
-
-    int sum = 0;
-
-    int& height = dp[idx][visit][side];
-    
-    if (height != -1) {
-        return height;
-    }    
-
-    for (int i = 0; i < N; i++) {
-        if ((visit & (1 << i))!=0) continue;        
-
-        if ((w >= box[i][1] && d >= box[i][2]) || (w >= box[i][2] && d >= box[i][1])) {
-            sum = box[i][0] + DFS(box[i][1], box[i][2], i, 0, visit | (1<<i));
-            if (height < sum) {
-                height = sum;
-            }
-        }
-        if ((w >= box[i][0] && d >= box[i][2]) || (w >= box[i][2] && d >= box[i][0])) {
-            sum = box[i][1] + DFS(box[i][0], box[i][2], i, 1, visit | (1<<i));
-            if (height < sum) {
-                height = sum;
-            }
-        }
-        if ((w >= box[i][0] && d >= box[i][1]) || (w >= box[i][1] && d >= box[i][0])) {
-            sum = box[i][2] + DFS(box[i][0], box[i][1], i, 2, visit | (1<<i));
-            if (height < sum) {
-                height = sum;
-            }
-        }
-    }
-    if (sum == 0) {
-        height = sum; 
-    }
-    return height; 
-}
-
-int main()
+int cal(int b, int use, int direc, int w, int d)
 {
-    int T;
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+	int& height = dp[b][use][direc];
+	
+	if (height != -1) {
+		return height;
+	}
+	int result = 0;
+	for (int i = 0; i < num_block; i++) {
+		if ((use & (1 << i)) != 0) {
+			continue;
+		}
+		if ((w >= blocks[i][1] && d >= blocks[i][2]) || (w >= blocks[i][2] && d >= blocks[i][1])) {
+			result = blocks[i][0] + cal(i, use | (1 << i), 0, blocks[i][1], blocks[i][2]);
+			if (height < result) {
+				height = result;
+			}
+		}
+		if ((w >= blocks[i][0] && d >= blocks[i][2]) || (w >= blocks[i][2] && d >= blocks[i][0])) {
+			result = blocks[i][1] + cal(i, use | (1 << i), 1, blocks[i][0], blocks[i][2]);
+			if (height < result) {
+				height = result;
+			}
+		}
+		if ((w >= blocks[i][1] && d >= blocks[i][0]) || (w >= blocks[i][0] && d >= blocks[i][1])) {
+			result = blocks[i][0] + cal(i, use | (1 << i), 1, blocks[i][1], blocks[i][0]);
+			if (height < result) {
+				height = result;
+			}
+		}
+	}
+	if (result == 0) {
+		height = 0;
+	}
+	return height;
+}
+int main() {
+	int T;
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
 
-    freopen("sos_input.txt", "r", stdin);
-
-    cin >> T;
-
-    for (int tc = 0; tc < T; tc++) {        
-        cin >> N;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < (1 << N)-1; j++) {
-                dp[i][j][0] = dp[i][j][1] = dp[i][j][2] = -1;
-            }
-            cin >> w >> l >> h;
-            box[i][0] = w;
-            box[i][1] = l;
-            box[i][2] = h;
-        }
-        cout << "#" << tc+1 << " " << DFS(10001,10001,0,0,0) << endl;
-    }
-    return 0;
+	cin >> T;
+	for (int tc = 0; tc < T; tc++) {
+		cin >> num_blocks;
+		for (int i = 0; i < num_blocks; i++) {
+			for (int k = 0; k < (1 << num_blocks); k++) {
+				dp[j][k][0] = dp[j][k][1] = dp[j][k][2] = -1;
+			}
+			cin >> blocks[j][0] >> blocks[j][1] >> blocks[j][2];
+		}
+		cout << "#" << tc + 1 << " " << cal(0, 0, 0, 10001, 10001) << endl;
+	}
 }
