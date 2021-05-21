792c99a123dbe6f11c4034f11dd5c386531ba089
Hanwool Lee <hanwool26@gmail.com>
2021-05-21 14:22:30 +0900

    Update tetris(5656).cpp

--- SWExpertAcademy/tetris(5656).cpp
+++ SWExpertAcademy/tetris(5656).cpp
@@ -4,16 +4,12 @@
 using namespace std;
 
 int N, W, H;
-int answer = 0; 
+int answer = 0;
 
-int map[15][12];
-int temp[15][12];
-bool visit[12];
+int dir_x[] = { -1, 1, 0, 0 };
+int dir_y[] = { 0, 0, -1, 1 };
 
-int dir_x[] = { -1,1,0,0 };
-int dir_y[] = { 0,0,-1,1 };
-
-void print_map(int (*temp)[12]) {
+void print_map(int(*temp)[20]) {
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
 			cout << temp[i][j] << " ";
@@ -23,34 +19,49 @@ void print_map(int (*temp)[12]) {
 	cout << "==============================" << endl;
 }
 
-int count_map(int(*temp)[12]) {
+int count_map(int(*temp)[20]) {
 	int cnt = 0;
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
-			if (map[i][j] > 0) cnt++;
+			if (temp[i][j] > 0) cnt++;
 		}
 	}
 	return cnt;
 }
 
-void DFS(int index, int cnt, int (*map)[12]) {
-	if (visit[index] == true) return;
+void map_refix(int(*temp)[20]){
+	int temp_contain;
+	for (int i = 0; i < W; i++){
+		for (int j = 0; j < H; j++){
+			if (temp[j][i] == 0){
+				for (int k = j; k > 0; k--){
+					temp_contain = temp[k][i];
+					temp[k][i] = temp[k - 1][i];
+					temp[k - 1][i] = temp_contain;
+				}
+			}
+		}
+	}
+}
+
+void DFS(int index, int cnt, int(*map)[20]) {
+
 	if (cnt == N) {
-		int count = count_map(map);
-		if (answer < count) {
+		int count = count_map(map);	
+		if (answer > count) {
 			answer = count;
 		}
-		return; 
-	}	
-	visit[index] = true;
-	memcpy(temp, map, sizeof(int)*12*15);
+		return;
+	}
+	int temp[20][20];
+	memcpy(temp, map, sizeof(temp));
 
-	print_map(temp);
-	queue<pair<int,int>> q;
-	int x, y,  step;
-	for (int i = 6; i < 7; i++) {	// W
+	queue<pair<int, int>> q;
+	int x, y, next_x, next_y, step;
+	for (int i = 0; i < W; i++) {	// W
 		// todo : implement queueing. 
-		
+		x = H-1;
+		y = i;		
 		for (int j = 0; j < H; j++) {	// H
 			if (map[j][i] == 0) continue;
 			else {
@@ -60,30 +71,34 @@ void DFS(int index, int cnt, int (*map)[12]) {
 			}
 		}
 		q.push(make_pair(x, y));
-		while (!q.empty()) {
-			int next_x = -1;
-			int next_y = -1;
+
+		while (!q.empty()) {			
 			x = q.front().first;
-			y = q.front().second;
+			y = q.front().second;	
+
 			q.pop();
-			printf("(%d, %d)\n", x, y);
-			
 			step = map[x][y];
-			map[x][y] = 0;
-			for (int i = 0; i < 4; i++) {
-				for (int j = 1; j < step; j++) {
-					next_x = x + dir_x[i] * j;
-					next_y = y + dir_y[i] * j;
+
+			if (step != 0){
+				map[x][y] = 0;
+				for (int i = 0; i < 4; i++) {
+					next_x = -1;
+					next_y = -1;
+					for (int j = 1; j < step; j++) {
+						next_x = x + dir_x[i] * j;
+						next_y = y + dir_y[i] * j;
+						if (next_x >= 0 && next_x < H && next_y >= 0 && next_y < H && \
+							map[next_x][next_y] != 0){
+							q.push(make_pair(next_x, next_y)); 
+						}
+					}
 				}
-				if (next_x<0 || next_x>H - 1 || next_y<0 || next_y>W - 1) continue;
-				q.push(make_pair(next_x, next_y));
 			}
 		}
-		DFS(i, cnt + 1, map);
-	}
-
-	memcpy(map, temp, sizeof(int) * 12 * 15);
-	visit[index] = false;
+		map_refix(map);	
+		DFS(i, cnt + 1, map);	
+		memcpy(map, temp, sizeof(temp));
+	}	
 }
 
 int main() {
@@ -95,9 +110,10 @@ int main() {
 	freopen("tetris.txt", "r", stdin);
 	cin >> T;
 	for (int tc = 0; tc < T; tc++) {
-		cin >> N >> W >> H;
-		memset(visit, false, sizeof(visit));
 
+		cin >> N >> W >> H;
+		answer = W*H;
+		int map[20][20];
 		for (int i = 0; i < H; i++) {
 			for (int j = 0; j < W; j++) {
 				cin >> map[i][j];
