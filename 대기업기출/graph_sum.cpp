69c596fa1b2eaa5e3e933ab3cd4ad4aae6aec8c5
Hanwool Lee <hanwool26@gmail.com>
2021-05-24 22:58:53 +0900

    Update graph_sum.cpp

--- 대기업기출/graph_sum.cpp
+++ 대기업기출/graph_sum.cpp
@@ -10,10 +10,27 @@
 **/
 
 #include <iostream>
+#include <stack>
 using namespace std;
 
 int N;
 long int graph[1001][1001];
+
+stack <int> s;
+
+void print_graph(){
+	for (int i = 0; i < N; i++){
+		cout.width(4);
+		cout << i+1;
+	}
+	cout << "\n\n";
+	for (int i = 1; i <= N; i++){
+		for (int j = 1; j <= N; j++){
+			cout << " | " << graph[i][j];
+		}
+		cout << endl << "---------------------------" << endl;
+	}
+}
 
 void init_case() {
 	for (int i = 0; i < 1001; i++) {
@@ -22,6 +39,30 @@ void init_case() {
 		}
 	}
 }
+
+
+void dfs(int vertex, int con_vertex, int sum, int visit[]){
+	printf("ver : %d con_ver : %d sum : %d\n", vertex, con_vertex, sum);
+	
+	if (visit[con_vertex] == true) return;
+	visit[con_vertex] = true;
+	int cur_node = con_vertex;
+
+	graph[vertex][con_vertex] = graph[con_vertex][vertex] = sum;
+	for (int col = 0; col <= N; col++){
+		if (col == vertex || visit[col] == true) continue;
+		if (graph[con_vertex][col] != 0){			
+			s.push(col);
+			cout << "push : " << col << endl;
+		}
+	}
+	while (!s.empty()){
+		con_vertex = s.top(); s.pop();
+		dfs(vertex, con_vertex, graph[cur_node][con_vertex]+sum, visit);
+	}
+
+}
+
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -33,9 +74,14 @@ int main() {
 	for (int tc = 0; tc < T; tc++) {
 		int i, j, w;
 		cin >> N;
-		for (int i = 0; i < N; i++) {
+		for (int k = 0; k < N; k++) {
 			cin >> i >> j >> w;
 			graph[i][j] = graph[j][i] = w;
 		}
+		int visit[8];
+		memset(visit, false, sizeof(visit));
+		dfs(1, 1, 0, visit);
+
+		print_graph();
 	}
 }
