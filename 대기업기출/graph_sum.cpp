4c649396332a3f8b05bbce0aaaf4325b43c23ddc
Hanwool Lee <hanwool26@gmail.com>
2021-05-26 10:13:26 +0900

    Update graph_sum.cpp

--- 대기업기출/graph_sum.cpp
+++ 대기업기출/graph_sum.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-#include <stack>
+#include <vector>
 #include <time.h>
+
 using namespace std;
 
+#define MAX 1001
 #define INF int(1e9)
 int N;
-long int graph[1001][1001];
-long int origin[1001][1001];
-bool visit[1001][1001];
+int graph[MAX][MAX];
+vector<int> v[MAX];
+bool visit[MAX];
 
 void print_graph() {
 	for (int i = 0; i < N; i++) {
@@ -30,7 +32,6 @@ void init_case() {
 		}
 	}
 }
-
 long int  graph_sum() {
 	long int sum = 0;
 	for (int i = 1; i < N; i++) {
@@ -41,76 +42,47 @@ long int  graph_sum() {
 	return sum;
 }
 
-void dfs(int start, int adj, int weight) {
-	if (visit[start][adj] == true) return;
-	//printf("before: (%d, %d), weight : %d\n", start, adj, weight);
-
-	if (graph[start][adj] > weight) {
-		graph[start][adj] = graph[adj][start] = weight;
-		weight = graph[adj][start];
-		visit[start][adj] = true;
+void dfs(int start, int adj, long int weight) {
+	graph[start][adj] = graph[adj][start] = weight;
+	int stack_size = v[adj].size();
+	visit[adj] = true;
+	for (int i = 0; i < stack_size; i++) {
+		int next = v[adj][i];
+		if (visit[next] == true) continue;
+		dfs(start, next, graph[adj][next] + weight);
 	}
-
-	printf("after: (%d, %d), weight : %d\n", start, adj, graph[adj][start]);
-	
-	stack <int> s;
-
-	for (int col = 1; col <= N; col++) {
-		if (col == adj || col == start) continue;
-		if (graph[adj][col] != INF) {
-			s.push(col);
-			//printf("pushed : %d\n", col);
-		}
-	}
-	visit[start][adj] = true;
-	while (!s.empty()) {
-		int node = s.top(); s.pop();
-		//printf("(%d %d) : %d\n", adj, node, graph[adj][node]);
-		dfs(start, node, graph[adj][node] + weight);
-	}
-	visit[start][adj] = false;
+	visit[adj] = false;
 }
+
 int main() {
+	clock_t start, end;
 	ios::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
-
-	clock_t start, end;
+	int T;
 
 	start = clock();
-	int T;
-	freopen("graph_sum1.txt", "r", stdin);
+	freopen("graph_sum.txt", "r", stdin);
 	cin >> T;
 	for (int tc = 0; tc < T; tc++) {
-		init_case();
-		int i, j, w;
+		int n_i, n_j, w;
 		cin >> N;
-		for (int k = 0; k < N; k++) {
-			cin >> i >> j >> w;
-			graph[i][j] = graph[j][i] = w;
-		}		
-		/*
-		for (int i = 1; i <= N; i++){
+		//init_case();
+		//memset(graph, INF, sizeof(int)*MAX*MAX);
+		for (int i = 0; i < N-1; i++) {
+			cin >> n_i >> n_j >> w;
+			graph[n_i][n_j] = graph[n_j][n_i] = w;
+			v[n_i].push_back(n_j); v[n_j].push_back(n_i);
+		}
+
+		for (int i = 1; i <= N; i++) {
+			visit[i] = true;
 			dfs(i, i, 0);
+			visit[i] = false;
 		}
-		*/
-
-		/* Fluyd washall algorithm */
-		/*
-		for (int k = 1; k <= N; k++){
-			for (int i = 1; i <= N; i++){
-				for (int j = 1; j <= N; j++){
-					if ((graph[i][j] > graph[i][k] + graph[k][j]) && (graph[i][k] != INF && \
-						graph[k][j] != INF)){
-						graph[i][j] = graph[i][k] + graph[k][j];
-					}
-				}
-			}
-		}
-		*/
-
+		//print_graph();
 		long int answer = graph_sum();
 		cout << "#" << tc + 1 << " " << answer << endl;
+		end = clock();
+		cout << "duration : " << (double)(end - start) / CLOCKS_PER_SEC << endl;
 	}
-	end = clock();
-	cout << "duration : " << (double)(end - start) / CLOCKS_PER_SEC;
 }
