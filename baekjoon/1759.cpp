b8be9d0a819cfc135977ad11a50e3b5a9cecdc09
Hanwool Lee <hanwool26@gmail.com>
2021-05-31 21:04:35 +0900

    Update 1759.cpp

--- baekjoon/1759.cpp
+++ baekjoon/1759.cpp
@@ -5,30 +5,33 @@ using namespace std;
 
 char alp[26];
 bool visit[26] = { false, };
-char save[26];
-char aei[5] = { 'a','e','i','o','u' };
+char save[15];
+char aei[5] = { 'a', 'e', 'i', 'o', 'u' };
 int N, R;
 int number = 0;
 
 void DFS(int idx, int cnt) {
 	if (cnt == R) {
 		save[cnt] = '\0';
+		int moum = 0;
 		for (int i = 0; i < R; i++) {
 			for (int j = 0; j < 5; j++) {
 				if (save[i] == aei[j]) {
-					cout << save << endl;
-					return;
+					moum++;
 				}
 			}
 		}
+		if (moum >= 1 && R - moum > 1){
+			cout << save << endl;
+			return;
+		}
 		return;
 	}
-	
 	for (int i = idx; i < N; i++) {
 		if (visit[i] == true) continue;
 		save[cnt] = alp[i];
 		visit[i] = true;
-		DFS(i+1, cnt + 1);
+		DFS(i + 1, cnt + 1);
 		visit[i] = false;
 	}
 }
@@ -41,5 +44,5 @@ int main() {
 		cin >> alp[i];
 	}
 	sort(alp, alp + N);
-	DFS(0,0);
+	DFS(0, 0);
 }
