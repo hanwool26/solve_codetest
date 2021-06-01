8e3030e7e9baa2bc5affbe0cac07f5b6c190485e
Hanwool Lee <hanwool26@gmail.com>
2021-06-01 10:36:40 +0900

    Update 2580.cpp

--- baekjoon/2580.cpp
+++ baekjoon/2580.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-typedef struct node{
+typedef struct node {
 	int x;
 	int y;
-	node(int a, int b){
+	node(int a, int b) {
 		x = a;
 		y = b;
 	}
@@ -18,34 +18,34 @@ bool visit[9][9] = { false, };
 vector<Node> v;
 bool answer = false;
 
-void print_board(){
-	for (int i = 0; i < 9; i++){
-		for (int j = 0; j < 9; j++){
+void print_board() {
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
 			cout << board[i][j] << " ";
 		}
 		cout << endl;
 	}
 }
 
-bool check_board(){
-	for (int i = 0; i < 9; i++){
+bool check_board() {
+	for (int i = 0; i < 9; i++) {
 		int dp[10] = { 0, };
-		for (int j = 0; j < 9; j++){
+		for (int j = 0; j < 9; j++) {
 			if (dp[board[i][j]] == 1) return false;
 			dp[board[i][j]] = 1;
 		}
 	}
-	for (int i = 0; i < 9; i++){
+	for (int i = 0; i < 9; i++) {
 		int dp[10] = { 0, };
-		for (int j = 0; j < 9; j++){
+		for (int j = 0; j < 9; j++) {
 			if (dp[board[j][i]] == 1) return false;
 			dp[board[j][i]] = 1;
 		}
 	}
-	for (int k = 0; k < 3; k++){
+	for (int k = 0; k < 3; k++) {
 		int dp[10] = { 0, };
-		for (int i = 3 * k; i < 3 * k + 3; i++){
-			for (int j = 3 * k; j < 3 * k + 3; j++){
+		for (int i = 3 * k; i < 3 * k + 3; i++) {
+			for (int j = 3 * k; j < 3 * k + 3; j++) {
 				if (dp[board[i][j]] == 1) return false;
 				dp[board[i][j]] = 1;
 			}
@@ -54,49 +54,57 @@ bool check_board(){
 	return true;
 }
 
-void DFS(int idx){
+bool back_tracking(int x, int y, int num) {
+	int x_idx = x / 3;
+	int y_idx = y / 3;
+
+	for (int i = x_idx * 3; i < x_idx * 3 + 3; i++) {
+		for (int j = y_idx * 3; j < y_idx * 3 + 3; j++) {
+			if (board[i][j] == num) return false;
+		}
+	}
+	for (int i = 0; i < 9; i++) {
+		if (board[x][i] == num || board[i][y] == num) {
+			return false;
+		}
+	}	
+	return true;
+}
+
+void DFS(int idx) {
 	if (answer) return;
-	if (idx == v.size() && check_board()){
+	if (idx == v.size() && check_board()) {
 		print_board();
 		answer = true;
 		return;
 	}
+	Node n = v[idx];
 
-	for (int i = 1; i < 10; i++){
-		Node n = v[idx];
-		int flag = false;
-		for (int j = 0; j < 9; j++){
-			if (board[n.x][j] == i || board[j][n.y] == i){
-				flag = true;
-				break;
-			}
-		}
-		if (flag == true) continue;
-		board[n.x][n.y] = i;		
+	for (int i = 1; i < 10; i++) {		
+		if (!back_tracking(n.x, n.y, i)) continue;
+		board[n.x][n.y] = i;
 		DFS(idx + 1);
 		board[n.x][n.y] = 0;
 	}
 }
 
-int main(){
+int main() {
 	clock_t start, end;
 
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
 	start = clock();
-	freopen("2580.txt", "r", stdin);
-	for (int i = 0; i < 9; i++){
-		for (int j = 0; j < 9; j++){
+	//freopen("2580.txt", "r", stdin);
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
 			cin >> board[i][j];
-			if (board[i][j] == 0){
+			if (board[i][j] == 0) {
 				v.push_back(Node(i, j));
 			}
 		}
 	}
-	//cout << check_board() << endl;
 	DFS(0);
 	end = clock();
-
 	cout << "duration : " << (double)(end - start) / CLOCKS_PER_SEC << endl;
 }
