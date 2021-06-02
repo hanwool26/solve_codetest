42c0a3431bdbb62807a6fdd111f9f041fc1ba357
Hanwool Lee <hanwool26@gmail.com>
2021-06-02 21:56:39 +0900

    Update 9019.cpp

--- baekjoon/9019.cpp
+++ baekjoon/9019.cpp
@@ -3,87 +3,86 @@
 #include <vector>
 #include <queue>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
 int A, B;
-enum oper{
-	D =1,
-	S,
-	L,
-	R
-};
-
-int dp[10000];
-
 
 unordered_map<int, char> op = { { 1, 'D' }, { 2, 'S' }, { 3, 'L' }, { 4, 'R' } };
-typedef struct node{
-	int num;
-	vector<char> v;
-	node(int a, vector<char> b, int c){
-		num = a;
-		if (c > 0){
-			b.push_back(op[c]);
-			v.assign(b.begin(), b.end());
-		}		
-	}
-}Node;
+
+typedef struct Visit{
+	int prev;
+	int state;
+}visit;
+
+visit v[10000];
+bool dp[10000];
+vector<int> result;
 
 void BFS(int num){
-
-	queue<Node> q;
-	vector<char> temp;
-	q.push(Node(num, temp, 0));
-
-
+	queue<int> q;
+	q.push(num);
+	
 	while (!q.empty()){
-		Node n = q.front();
-		q.pop();
-		int base_num = n.num;
+		int base_num = q.front();
 
+		q.pop();
 		if (base_num == B){
-			//cout << base_num << endl;
-			for (auto &i : n.v){
-				cout << i;
+			//cout << A << " " << B << endl;
+			while (base_num != A){
+				result.push_back(v[base_num].state);
+				base_num = v[base_num].prev;
 			}
-			cout << endl;
 			break;
 		}
 		for (int i = 1; i <= 4; i++){
 			if (i == 1){
 				num = base_num * 2;
-				if (num > 10000) num = num % 10000;
+				if (num > 9999) num = num % 10000;
 			}
-			if (i == 2){
-				num = base_num - 1;
-				if (num == 0) num = 9999;
+			else if (i == 2){
+				if (base_num == 0) num = 9999;
+				else num = base_num - 1;
 			}
-			if (i == 3){
+			else if (i == 3){
 				num = (base_num % 1000) * 10 + base_num / 1000;
 			}
-			if (i == 4){
-				num = (base_num & 10) * 1000 + base_num / 10;
+			else if (i == 4){
+				num = (base_num % 10) * 1000 + base_num / 10;
 			}
-		//	if (dp[num] == 1) continue;
-		//	dp[num] = 1;
-			q.push(Node(num, n.v, i));
+			if (dp[num] != true){
+				dp[num] = true;
+				v[num].prev = base_num;
+				v[num].state = i;
+				q.push(num);
+			}
 		}
 	}
-
 }
 
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	freopen("9019.txt", "r", stdin);
+	//freopen("9019.txt", "r", stdin);
+
 	int T;
 	cin >> T;
 	for (int tc = 0; tc < T; tc++){
-		for (int i = 0; i < 10000; i++){
-			dp[i] = 0;
-		}
 		cin >> A >> B;
+        
+		for (int i = 0; i < 10000; i++){
+			v[i].prev = -1;
+            v[i].state = 0;
+			dp[i] = false;
+		}
+        
+		result.clear();
 		BFS(A);
+		reverse(result.begin(), result.end());
+		for (auto &i : result){
+			cout << op[i];
+		}
+		cout << endl;
 	}
 }
