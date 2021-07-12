d1b537125025e94a4abb98002098ad53880af2cd
Hanwool Lee <hanwool26@gmail.com>
2021-07-12 22:57:43 +0900

    Update 9999(광고).cpp

--- SWExpertAcademy/9999(광고).cpp
+++ SWExpertAcademy/9999(광고).cpp
@@ -1,35 +1,60 @@
-#include <iostream>
+#include<iostream>
+#define MAX_NUM 100001
+
+int A[MAX_NUM] = { 0 };
+int B[MAX_NUM] = { 0 };
+int S[MAX_NUM] = { 0 };
+
 using namespace std;
 
-int L, N;
-int peak[(int)1e8];
-int peak_end;
-
-void print_peak(){
-	for (int i = 0; i < peak_end; i++){
-		cout << peak[i] << " ";
-	}
-	cout << endl;
+int max(int a, int b)
+{
+	return a > b ? a : b;
 }
 
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-
+int main(int argc, char** argv)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int test_case;
+	int T;
 	freopen("9999.txt", "r", stdin);
-	int T; cin >> T;
+	cin >> T;
 
-	for (int tc = 1; tc <= T; tc++){
-		cin >> L >>  N;
-		for (int i = 0; i < N; i++){
-			int s, e;
-			cin >> s >> e;
-			for (int i = s; i < e; i++){
-				peak[i] = 1;
-				peak_end = e;
-			}
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int N, L;
+		int max_total = 0;
+
+
+		cin >> L;
+		cin >> N;
+
+		for (int i = 0; i < N; i++)
+		{
+			cin >> A[i] >> B[i];
+			S[i + 1] = S[i] + B[i] - A[i];
 		}
 
-		print_peak();
+		int s = 0;
+		int e = 0;
+		while (e < N)
+		{
+			while (s <= e && A[s] + L <= B[e])
+			{
+				int len = A[s] + L - A[e];
+				if (len > 0)
+					max_total = max(max_total, S[e] - S[s] + len);
+				else
+					max_total = max(max_total, S[e] - S[s]);
+				s++;
+			}
+			e++;
+		}
+		max_total = max(max_total, S[N] - S[s]);
+
+		cout << "#" << test_case << " " << max_total << endl;
 	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
