0e14f7c88aa272b3953c8341282d6f4918e7ba27
Hanwool Lee <hanwool26@gmail.com>
2021-07-15 22:54:15 +0900

    Update 1726(로봇).cpp

--- baekjoon/1726(로봇).cpp
+++ baekjoon/1726(로봇).cpp
@@ -1,100 +1,108 @@
 #include <iostream>
 #include <queue>
+#include <cstring>
+
 using namespace std;
 
-int R, C;
-int map[101][101];
-bool visit[101][101] = { false, };
+#define MAX 101
 
-int dir_x[] = { 0, 0, 1, -1 };
-int dir_y[] = { 1, -1, 0, 0 };	// E W S N
+int map[101][101];
+int visit[101][101][5];
+int M, N;
+
+int dir_x[] = { 0, 0, 0, 1, -1 };
+int dir_y[] = { 0, 1, -1, 0, 0 };
 
 typedef struct node{
 	int x;
 	int y;
 	int dir;
-	int cnt;
+	int depth;
 	node(int a, int b, int c, int d){
 		x = a;
 		y = b;
 		dir = c;
-		cnt = d;
+		depth = d;
 	}
 }Node;
-Node s(0,0,0,0), e(0,0,0,0);
 
-enum dir{
-	E = 1,
-	W,
-	S,
-	N
-};
 
-int answer = 0;
+// 1 : 동, 2 : 서, 3 : 남, 4 : 북
 
-void print_map(){
-	for (int i = 1; i <= R; i++){
-		for (int j = 1; j <= C; j++){
-			cout << map[i][j] << " ";
-		}
-		cout << endl;
-	}
-}
-
-void BFS(Node n){
+int BFS(Node s, Node e){
+	int answer = 0;
 	queue<Node> q;
-	int xx, yy, nx, ny;
-	int cur_dir, next_dir;
-	int cnt;
-	q.push(Node(s.x, s.y, s.dir, s.cnt));
+	q.push(Node(s.x, s.y, s.dir, s.depth));
+
+	visit[s.x][s.y][s.dir] = true;
 
 	while (!q.empty()){
-		Node n = q.front();
-		xx = n.x;
-		yy = n.y;
-		cur_dir = n.dir;
-		cnt = n.cnt;
+		Node temp = q.front();
 		q.pop();
-		visit[xx][yy] = true;
-		if (e.x == xx && e.y == yy){
-			if (e.dir != cur_dir) answer = cnt + 1;
-			else answer = cnt;
+
+		int xx = temp.x;
+		int yy = temp.y;
+		int dir = temp.dir;
+		int depth = temp.depth;
+
+		if (xx == e.x && yy == e.y && dir == e.dir){
+			answer = depth;
 			break;
 		}
-		 
-		for (int i = 0; i < 4; i++){
-			nx = xx + dir_x[i];
-			ny = yy + dir_y[i];
-			if (nx < 1 || nx > R || ny < 1 || ny > C || map[nx][ny] || visit[nx][ny]) continue;
-			if (cur_dir != i){
-				q.push(node(nx, ny, i, cnt + 2));
+		// go 
+		for (int i = 1; i <= 3; i++){
+			int nx = xx + dir_x[dir] * i;
+			int ny = yy + dir_y[dir] * i;
+			if (nx < 1 || nx > M || ny < 1 || ny > N || map[nx][ny] == 1) break;
+			if (visit[nx][ny][dir]) continue;
+			q.push(Node(nx, ny, dir, depth + 1));
+			visit[nx][ny][dir] = true;
+		}
+
+		// turn left or right
+		if (dir == 1 || dir == 2){
+			if (visit[xx][yy][3] == false){
+				q.push(Node(xx, yy, 3, depth + 1));
+				visit[xx][yy][3] = true;
 			}
-			else{
-				q.push(node(nx, ny, i, cnt));
+			if (visit[xx][yy][4] == false){
+				q.push(Node(xx, yy, 4, depth + 1));
+				visit[xx][yy][4] = true;
 			}
-		}		
-	}	
+
+		}
+		if (dir == 3 || dir == 4){
+			if (visit[xx][yy][1] == false){
+				q.push(Node(xx, yy, 1, depth + 1));
+				visit[xx][yy][1] = true;
+			}
+			if (visit[xx][yy][2] == false){
+				q.push(Node(xx, yy, 2, depth + 1));
+				visit[xx][yy][2] = true;
+			}
+		}
+	}
+	return answer;
 }
 
 int main(){
 	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	int x, y, dir;
+	cin.tie(0); cout.tie(0);
 
 	freopen("1726.txt", "r", stdin);
-	cin >> R >> C;
-	for (int i = 1; i <= R; i++){
-		for (int j = 1; j <= C; j++){
+	memset(visit, false, sizeof(visit));
+	cin >> M >> N;
+	for (int i = 1; i <= M; i++){
+		for (int j = 1; j <= N; j++){
 			cin >> map[i][j];
 		}
 	}
-	cin >> s.x >> s.y >> s.dir;
-	cin >> e.x >> e.y >> e.dir;
-
-	//print_map();
-
-	BFS(s);
-	cout << answer << endl;
-
-	return 0;
+	
+	int x, y, dir, depth = 0;
+	cin >> x >> y >> dir;
+	Node s(x, y, dir, depth);
+	cin >> x >> y >> dir;
+	Node e(x, y, dir, depth);
+	
+	cout << BFS(s, e) << endl;	
 }
