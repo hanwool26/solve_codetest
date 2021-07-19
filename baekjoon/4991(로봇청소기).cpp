5ec057c86f23bffef025a92355cfd91cf6a787b3
Hanwool Lee <hanwool26@gmail.com>
2021-07-19 23:08:20 +0900

    Update 4991(로봇청소기).cpp

--- baekjoon/4991(로봇청소기).cpp
+++ baekjoon/4991(로봇청소기).cpp
@@ -1,99 +1,141 @@
 #include <iostream>
+#include <vector>
 #include <queue>
 #include <cstring>
 using namespace std;
 
-int C, R;
 char map[20][20];
-int visit[10][20][20];
-
-int dir_x[] = { -1, 1, 0, 0 };
-int dir_y[] = { 0, 0, -1, 1 };
-
-int dirty_cnt;
-int answer;
+int C, R;
 
 typedef struct node{
 	int x;
 	int y;
 	int depth;
-	node(int a, int b, int  c){
+	node(int a, int b, int c){
 		x = a;
 		y = b;
 		depth = c;
 	}
 }Node;
-Node s(0, 0, 0);
 
-
-void DFS(int x, int y, int cnt, int dis_cnt){
-	if (cnt == dirty_cnt){
-		if (answer > dis_cnt){
-			answer = dis_cnt;
-		}
-		return;
+typedef struct edge{
+	int adj;
+	int dis;
+	edge(int a, int b){
+		adj = a;
+		dis = b;
 	}
-	visit[cnt][x][y] = true;
-	queue<Node> q;
-	q.push((Node(x, y, 0)));
-	int xx, yy, nx, ny;
-	int cur_depth;
-	while (!q.empty()){
-		Node cur = q.front();
-		xx = cur.x;
-		yy = cur.y;
-		cur_depth = cur.depth;
-		q.pop();
-		if (map[xx][yy] == '*' && !visit[cnt][xx][yy]){
-			map[xx][yy] = '.';
-			DFS(xx, yy, cnt + 1, dis_cnt + cur_depth);
-			break;
+	bool operator<(const edge &b)const{
+		return dis > b.dis;
+	}
+}Edge;
+
+Node start(Node(0, 0, 0));
+vector<Node> dv; // dirty vector
+int dir_x[] = { -1, 1, 0, 0 };
+int dir_y[] = { 0, 0, -1, 1 };
+
+int graph[20][20];
+vector<Edge> de[100];
+
+void print_graph(){
+	for (int i = 0; i<dv.size(); i++){
+		for (int j = 0; j<dv.size(); j++){
+			cout << graph[i][j] << " ";
 		}
-		visit[cnt][xx][yy] = true;
-		for (int i = 0; i < 4; i++){
-			nx = xx + dir_x[i];
-			ny = yy + dir_y[i];
-			if (nx < 0 || nx > R - 1 || ny < 0 || ny > C - 1 || visit[cnt][nx][ny] || \
-				map[nx][ny] == 'x') continue;
-			q.push(Node(nx, ny, cur_depth + 1));
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void make_graph(){
+	queue<Node> q;
+	int visit[20][20];
+	for (int i = 0; i<dv.size(); i++){
+		memset(visit, false, sizeof(visit));
+		q.push(Node(dv[i].x, dv[i].y, dv[i].depth));
+		visit[dv[i].x][dv[i].y] = true;
+
+		while (!q.empty()){
+			Node temp = q.front();
+			q.pop();
+			int x = temp.x;
+			int y = temp.y;
+			int depth = temp.depth;
+			for (int k = 0; k<dv.size(); k++){
+				if (x == dv[k].x && y == dv[k].y){
+						de[k].push_back(Edge(i, depth));
+						de[i].push_back(Edge(k, depth));
+						graph[i][k] = graph[k][i] = depth;
+				
+				}
+			}
+			for (int i = 0; i<dv.size(); i++){
+				int nx = x + dir_x[i];
+				int ny = y + dir_y[i];
+				if (nx < 0 || nx > R - 1 || ny < 0 || ny > C - 1 || visit[nx][ny] || map[nx][ny] == 'x') continue;
+				q.push(Node(nx, ny, depth + 1));
+				visit[nx][ny] = true;
+			}
 		}
 	}
 }
 
+int prim_mst(){
+	if (de[0].size() != dv.size()) return -1;
+	int answer = 0;
+	bool visit[10];
+	memset(visit, false, sizeof(visit));
+	priority_queue<Edge> pq;
+
+	pq.push(Edge(0, 0));
+
+	while (!pq.empty()){
+		Edge temp = pq.top();
+		pq.pop();
+		int adj = temp.adj;
+		int dis = temp.dis;
+		if (visit[adj] != true){
+			visit[adj] = true;
+			answer += dis;
+			for (int i = 0; i < de[adj].size(); i++){
+				int next = de[adj][i].adj;
+				int n_next = de[adj][i].dis;
+				pq.push(Edge(next, n_next));
+			}
+		}
+
+	}
+	return answer;
+}
+
 int main(){
 	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	char temp[20];
+	cin.tie(0); cout.tie(0);
+
 	freopen("4991.txt", "r", stdin);
+
 	while (true){
 		cin >> C >> R;
 		if (C == 0 && R == 0) break;
-		answer = int(1e9);
-		for (int i = 0; i < 10; i++){
-			for (int j = 0; j < 20; j++){
-				for (int k = 0; k < 20; k++){
-					visit[i][j][k] = false;
+		for (int i = 0; i<R; i++){	
+			char temp[20];
+				cin >> temp;
+				for (int j = 0; j < strlen(temp); j++){
+					map[i][j] = temp[j];
+					if (map[i][j] == 'o'){
+						start.x = i;
+						start.y = j;
+					}
+					if (map[i][j] == '*' || map[i][j] == 'o'){
+						dv.push_back(Node(i, j, 0));
+					}
 				}
-			}
+			
 		}
-		dirty_cnt = 0;
-		if (C == 0 && R == 0) break;
-		for (int i = 0; i < R; i++){
-			cin >> temp;
-			for (int j = 0; j < C; j++){
-				map[i][j] = temp[j];
-				if (map[i][j] == 'o'){
-					s.x = i;
-					s.y = j;
-					s.depth = 0;
-				}
-				else if (map[i][j] == '*') dirty_cnt++;
-			}
-		}
-		DFS(s.x, s.y, 0, s.depth);
-		if (answer != int(1e9)) cout << answer << endl;
-		else cout << -1 << endl;
-
+		make_graph();
+		print_graph();
+		cout << prim_mst() << endl;
 	}
-	return 0;
+
 }
