07a01cd2b0cd7d7b9e28654cbebbf9cc66c68f64
Hanwool Lee <hanwool26@gmail.com>
2021-07-20 15:54:08 +0900

    Update 4991_graph.cpp

--- baekjoon/4991_graph.cpp
+++ baekjoon/4991_graph.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <cstring>
+#include <time.h>
 using namespace std;
 
 char map[20][20];
@@ -35,8 +36,20 @@ vector<Node> dv; // dirty vector
 int dir_x[] = { -1, 1, 0, 0 };
 int dir_y[] = { 0, 0, -1, 1 };
 
+int start_idx;
 int graph[20][20];
+
 vector<Edge> de[100];
+
+void print_graph() {
+	for (int i = 0; i < dv.size(); i++) {
+		for (int j = 0; j < dv.size(); j++) {
+			cout << graph[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
 
 void make_graph() {
 	queue<Node> q;
@@ -45,7 +58,7 @@ void make_graph() {
 		memset(visit, false, sizeof(visit));
 		q.push(Node(dv[i].x, dv[i].y, dv[i].depth));
 		visit[dv[i].x][dv[i].y] = true;
-
+		if (dv[i].x == start.x && dv[i].y == start.y) start_idx = i;
 		while (!q.empty()) {
 			Node temp = q.front();
 			q.pop();
@@ -53,7 +66,9 @@ void make_graph() {
 			int y = temp.y;
 			int depth = temp.depth;
 			for (int k = 0; k < dv.size(); k++) {
+				
 				if (x == dv[k].x && y == dv[k].y) {
+					
 					de[i].push_back(Edge(k, depth));
 					graph[i][k] = depth;
 				}
@@ -66,6 +81,25 @@ void make_graph() {
 				visit[nx][ny] = true;
 			}
 		}
+	}
+}
+
+
+int answer;
+bool visit[10];
+
+void DFS(int idx, int dis, int cnt) {
+	
+	if (dis > answer || idx > dv.size()) return;
+	if (cnt == dv.size()) {
+		answer = min(answer, dis);
+		return;
+	}
+	for (int i = 0; i < dv.size(); i++) {
+		if (visit[i]) continue;
+		visit[i] = true;
+		DFS(i, dis + graph[idx][i], cnt + 1);
+		visit[i] = false;
 	}
 }
 
@@ -103,24 +137,46 @@ int prim_mst() {
 }
 
 int main() {
-	freopen("4991.txt", "r", stdin);
+	clock_t start_t, end_t;
+	start_t = clock();
+	//freopen("4991.txt", "r", stdin);
 	while (true) {
 		cin >> C >> R;
 		if (C == 0 && R == 0) break;
 		dv.clear();
+		for (int i = 0; i < 100; i++) {
+			de[i].clear();
+		}
 		for (int i = 0; i < R; i++) {
 			char temp[100];
 			cin >> temp;
 			for (int j = 0; j < strlen(temp); j++) {
 				map[i][j] = temp[j];
+				if (map[i][j] == 'o') {
+					start.x = i;
+					start.y = j;
+				}
 				if (map[i][j] == '*' || map[i][j] == 'o') {
 					dv.push_back(Node(i, j, 0));
 				}
 			}
 		}
 		make_graph();
-		int answer = prim_mst();
-		cout << answer << endl;
-	}
+		//print_graph();
+		//int answer = prim_mst();
+		memset(visit, false, sizeof(visit));
+		answer = (int)1e9;
 
+		DFS(start_idx, 0, 0);
+		
+		int de_size = de[0].size();
+		if (de_size != dv.size()) answer = -1;
+		for (int i = 0; i < de_size; i++) {
+			de[i].clear();
+		}
+
+		cout << answer << endl;
+		end_t = clock();
+	}
+	//cout << "duration : " << (double)(end_t - start_t) / CLOCKS_PER_SEC << endl;
 }
