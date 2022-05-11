b828394f63b2508b8e3bc7e60713fba4cf7f94be
Hanwool Lee <hanwool26@gmail.com>
2022-05-11 14:26:35 +0900

    Update Kruskal_MST.cpp

--- basic_algorithm/Kruskal_MST.cpp
+++ basic_algorithm/Kruskal_MST.cpp
@@ -1,56 +1,86 @@
-# 최소 스패닝 트리. & Union/Find 활용.
+// https://swblossom.tistory.com/51
+/*
+7
+1 2 4
+1 3 2
+2 4 4
+2 5 5
+3 5 4
+4 6 1
+5 6 2
+*/
 
 #include <iostream>
-#include <algorithm>
-#include <queue>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
-int unf[1001];
 
-struct Edge{
-	int v1;
-	int v2;
-	int val;
-	Edge(int a, int b, int c){
-		v1 = a;
-		v2 = b;
-		val = c;
+typedef struct node {
+	int s;
+	int e;
+	int dis;
+	node(int s, int e, int dis) {
+		this->s = s;
+		this->e = e;
+		this->dis = dis;
 	}
-	bool operator<(Edge &b){
-		return val < b.val;
+	bool operator<(node& b) {
+		return this->dis < b.dis;
 	}
-};
+}Node;
 
-int Find(int v){
-	if(v==unf[v]) return v;
-	else return unf[v]=Find(unf[v]);
+int parent[100];
+int N, res;
+vector<Node> v;
+
+void init() {
+	res = 0;
+	for (int i = 0; i < 100; i++) {
+		parent[i] = i;
+	}
 }
 
-void Union(int a, int b){
+int Find(int x) {
+	if (parent[x] == x) return x;
+	else {
+		return parent[x] = Find(parent[x]);
+	}
+}
+
+void Union(int a, int b) {
 	a = Find(a);
 	b = Find(b);
-	if(a!=b) unf[a] = b;
+	if (a != b) {
+		parent[a] = b;
+	}
 }
 
-int main(){
-	vector<Edge> ed;
-	for(int i = 1; i<=N; i++){
-		unf[i] = i;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	freopen("다익스트라.txt", "r", stdin);
+	cin >> N;
+	int s, e, dis;
+
+	init();
+	for (int i = 0; i < N; i++) {
+		cin >> s >> e >> dis;
+		v.push_back(Node(s, e, dis));
 	}
-	
-	for(i=1; i<=M; i++){
-		ed.push_back(Edge(a,b,c));
-	}
-	sort(ed.begin(), ed.end());
-	
-	for(i=0; i<M; i++){
-		int fa = Find(ed[i].v1);
-		int fb = Find(ed[i].v2);
-		if(fa!fb){
-			res+=ed[i].val;
-			Union(ed[i].v1, ed[i].v2);
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < N; i++) {
+		Node n = v[i];
+
+		int ed_s = Find(n.s);
+		int ed_e = Find(n.e);
+		if (ed_s != ed_e) {
+			res += n.dis;
+			parent[ed_s] = ed_e;
 		}
 	}
+	cout << res << endl;
 	return 0;
 }
