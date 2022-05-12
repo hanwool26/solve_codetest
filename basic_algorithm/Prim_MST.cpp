ae8e0448f9d98065c76460af5e9ebe700292e7fb
Hanwool Lee <hanwool26@gmail.com>
2022-05-12 10:02:44 +0900

    Update Prim_MST.cpp

--- basic_algorithm/Prim_MST.cpp
+++ basic_algorithm/Prim_MST.cpp
@@ -1,46 +1,70 @@
+/*
+10
+1 7 12
+4 7 20
+2 4 21
+1 4 23
+1 2 26
+3 5 29
+5 6 30
+2 3 36
+3 6 37
+2 5 45
+3 7 55
+*/
+
 #include <iostream>
-#include <algorithm>
-#include <queue>
 #include <vector>
+#include <queue>
 
 using namespace std;
-int unf[1001];
 
-struct Edge{
-	int  e;
-	int val;
-	Edge(int a, int b, ){
-		e = a;
-		val = c;
+typedef struct edge {
+	int e;		// end
+	int dis;	// distance
+	edge(int e, int dis) {
+		this->e = e;
+		this->dis = dis;
 	}
-	bool operator<(const Edge &b)const{
-		return val > b.val;	// 최소 heap 
+	bool operator<(const edge& e) const{
+		return this->dis > e.dis;
 	}
-};
+}Edge;
 
-int main(){
-	priorty_queue<Edge> Q;
-	vector<pair<int, int>> map[30];
-	for(i=1; i<=m; i++){	// m is the number of edges.
-		map[a].push_back(make_pair(b,c));
-		map[b].push_back(make_pair(a,c));
+bool visit[10];
+
+int main() {
+	vector<Edge> graph[10];
+
+	freopen("다익스트라.txt", "r", stdin);
+	int N; cin >> N;
+	int s, e, dis;
+	for (int i = 0; i < N; i++) {
+		cin >> s >> e >> dis;
+		graph[s].push_back(Edge(e, dis));
+		graph[e].push_back(Edge(s, dis));
 	}
-	
-	Q.push(Edge(1,0));
-	while(!Q.empty()){
-		Edge tmp = Q.top();
-		Q.pop();
-		
-		int v = temp.e;
-		int cot = tmp.val;
-		if(ch[v]==0){
-			answer += cost;
-			ch[v] =1;
-			for(i=0; i<map[v].size(); i++){
-				Q.push(Edge(map[v][i].first, map[v][i].second));
+	memset(visit, false, sizeof(visit));
+	priority_queue<Edge> pq;
+
+	pq.push(Edge(1, 0));
+	int res = 0;	
+
+	while (!pq.empty()) {
+		int e = pq.top().e;
+		int dis = pq.top().dis;
+		pq.pop();
+
+		if (visit[e] != true) {
+			res += dis;
+			visit[e] = true;
+			for (int i = 0; i < graph[e].size(); i++) {
+				int next = graph[e][i].e;
+				int next_dis = graph[e][i].dis;
+				pq.push(Edge(next, next_dis));
 			}
-		}
-		return 0;
+		}		
 	}
-}
-		
+	cout << res << endl; // 148
+	return 0;	
+}	
