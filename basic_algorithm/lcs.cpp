bfd7239fc696b58bc281dbda2e57585f52b82d9f
Hanwool Lee <hanwool26@gmail.com>
2022-05-12 13:45:58 +0900

    Update lcs.cpp

--- basic_algorithm/lcs.cpp
+++ basic_algorithm/lcs.cpp
@@ -1,7 +1,7 @@
 // https://www.crocus.co.kr/787
-
 #include <iostream>
 #include <string>
+#include <stack>
 
 using namespace std;
 
@@ -11,9 +11,10 @@ int main() {
 	string str1 = "ACAYKP";
 	string str2 = "CAPCAK";
 
+
 	for (int i = 1; i <= str1.length(); i++) {
 		for (int j = 1; j <= str2.length(); j++) {
-
+			
 			if (str1[i-1] == str2[j-1]) {
 				lcs[i][j] = lcs[i - 1][j - 1] + 1;
 			}
@@ -28,6 +29,25 @@ int main() {
 		}
 		cout << endl;		
 	}
-	cout << lcs[str1.length()][str2.length()] << endl; // 4 : ACAK
+	cout << lcs[str1.length()][str2.length()] << endl; // 4, ACAK
+
+	stack<int> st;
+	int i = str1.length();
+	int j = str2.length();
+
+	while (lcs[i][j]!=0) {
+		if (lcs[i][j] == lcs[i][j - 1]) j--;
+		else if (lcs[i][j] == lcs[i - 1][j]) i--;
+		else if (lcs[i][j] - 1 == lcs[i - 1][j - 1]) {
+			st.push(i-1);
+			i--;
+			j--;
+		}
+	}
+
+	while (!st.empty()) {
+		cout << str1[st.top()];
+		st.pop();
+	} // ACAK
 	return 0;
 }
