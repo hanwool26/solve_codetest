743700885fa0c5c2dc434da14a3bccc6af185a91
Hanwool Lee <hanwool26@gmail.com>
2022-05-17 13:46:15 +0900

    Update pipe_connection(4340).cpp

--- SWExpertAcademy/pipe_connection(4340).cpp
+++ SWExpertAcademy/pipe_connection(4340).cpp
@@ -1,119 +1,94 @@
 #include <iostream>
+#include <vector>
+#include <cstring>
 using namespace std;
 
+int N;
 int map[50][50];
 int visit[50][50];
 int dp[50][50][4];
-int N;
-int answer = 0;
+int answer;
 
-
-void init_case() {
-	answer = N * N;
+void init_test() {	
 	for (int i = 0; i < 50; i++) {
 		for (int j = 0; j < 50; j++) {
 			visit[i][j] = 0;
-			for (int dir = 0; dir < 4; dir++){
-				dp[i][j][dir] = answer;
+			for (int k = 0; k < 4; k++) {
+				dp[i][j][k] = 0;
 			}
 		}
 	}
-}	
+	
+}
+/* Pipe : */
+#define LEFT 0
+#define RIGHT 1
+#define UP 2
+#define DOWN 3
+/*
+dir : Input 방향
+*/
+void dfs(int x, int y, int dir, int cnt) {
+	
+	if (x < 0 || y < 0 || x > N - 1 || y > N - 1 || visit[x][y]) return;
+	int cur_pipe = map[x][y];
+	if (cur_pipe == 0) return;
 
-enum direction {
-	UP,
-	DOWN,
-	LEFT,
-	RIGHT
-};
-
-void DFS(int x, int y, int dir, int cnt) {
-	int pipe = map[x][y];
-	if (x < 0 || x > N - 1 || y < 0 || y > N - 1 || pipe == 0 || visit[x][y] == 1) return;
-	if (dp[x][y][dir] < cnt) return;
-
-	if (x == N - 1 && y == N - 1 && ((pipe == 1 && dir == RIGHT) || pipe == 6 && dir == DOWN) && cnt!=1) {
-		if (answer > cnt) {
-			answer = cnt;
-		}
-		return;
+	if (x == N - 1 && y == N - 1 && 
+		((dir == RIGHT && cur_pipe <= 2 ) || (dir == DOWN && cur_pipe > 2))) {
+		if (answer > cnt) answer = cnt;
+		return; 
 	}
-
-	if (x == 0 && y == 0 && ((pipe == 1 && dir == LEFT) || pipe == 4 && dir == UP) && cnt!=1) {
-		if (answer > cnt) {
-			answer = cnt;
-		}
-		return;
-	}
-
-	int next_x, next_y;
-	visit[x][y] = 1;
-	dp[x][y][dir] = cnt;
-
-	if (pipe <= 2) {
-		if (dir == UP) {
-			next_x = x - 1;
-			next_y = y;
-			DFS(next_x, next_y, UP, cnt + 1);
-		}
-		if (dir == DOWN) {
-			next_x = x + 1;
-			next_y = y;
-			DFS(next_x, next_y, DOWN, cnt + 1);
-		}
-		if (dir == LEFT) {
-			next_x = x;
-			next_y = y - 1;
-			DFS(next_x, next_y, LEFT, cnt + 1);
-		}
-		if (dir == RIGHT) {
-			next_x = x;
-			next_y = y + 1;
-			DFS(next_x, next_y, RIGHT, cnt + 1);
-		}
-	}
+	
+	if (dp[x][y][dir] == 0) dp[x][y][dir] = cnt;
 	else {
-		if (dir == UP || dir == DOWN) {
-			next_x = x;
-			next_y = y + 1;
-			DFS(next_x, next_y, RIGHT, cnt + 1);
-			next_y = y - 1;
-			DFS(next_x, next_y, LEFT, cnt + 1);
+		if (dp[x][y][dir] > cnt) dp[x][y][dir] = cnt;
+		else return;
+	}
+	visit[x][y] = true;
+
+	if (dir == LEFT || dir == RIGHT)
+	{		
+		if (cur_pipe > 2) {
+			dfs(x - 1, y, UP, cnt + 1);
+			dfs(x + 1, y, DOWN, cnt + 1);
 		}
-		if (dir == LEFT || dir == RIGHT) {
-			next_x = x + 1;
-			next_y = y;
-			DFS(next_x, next_y, DOWN, cnt + 1);
-			next_x = x - 1;
-			next_y = y;
-			DFS(next_x, next_y, UP, cnt + 1);
+		else {
+			dfs(x, y - 1, LEFT, cnt + 1);
+			dfs(x, y + 1, RIGHT, cnt + 1);
 		}
 	}
-	visit[x][y] = 0;
+	if (dir == UP || dir == DOWN) {
+		if (cur_pipe > 2) { 
+			dfs(x, y - 1, LEFT, cnt + 1);
+			dfs(x, y + 1, RIGHT, cnt + 1);
+		}
+		else {
+			dfs(x-1, y, UP, cnt + 1);	
+			dfs(x + 1, y, DOWN, cnt + 1);
+		}
+	}
+	visit[x][y] = false;
 }
 
 int main() {
-	int T;
 	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-	freopen("pipe_connection_input.txt", "r", stdin);
-	cin >> T;
-
-	for (int tc = 0; tc < T; tc++) {
+	cin.tie(0);
+	freopen("pipe.txt", "r", stdin);
+	int T; cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
 		cin >> N;
-		init_case();
+		init_test();
+		answer = 1e9;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				cin >> map[i][j];
 			}
 		}
+		dfs(0, 0, RIGHT, 1);
+		init_test();
+		dfs(N - 1, N - 1, LEFT, 1);
 
-		DFS(0, 0, RIGHT, 1);
-		int a = answer;
-		init_case();
-		DFS(N - 1, N - 1, LEFT, 1);
-		if (answer > a) answer = a;
-
-		cout << "#" << tc + 1 << " " << answer << endl;
-	}	
+		cout << "#" << tc << " " << answer << endl;
+	}
 }
