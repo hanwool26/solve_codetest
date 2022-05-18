34914d17cb8220bbb285cfa5b9d9a25b7bf44113
Hanwool Lee <hanwool26@gmail.com>
2022-05-18 17:36:03 +0900

    Update 오래된_휴대폰.cpp

--- SWExpertAcademy/오래된_휴대폰.cpp
+++ SWExpertAcademy/오래된_휴대폰.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 int answer;
@@ -7,25 +6,56 @@ int N, O, M, W;
 int num[10];
 bool op[5]; // 1: '+', 2: '-', 3: '*', 4: '/' 
 
-int dp[999];
+int dp[1000];
+int origin[1000];
 
 void init_test() {
-	answer = 0;
+	answer = 21;
 	memset(dp, 0, sizeof(dp));
 	memset(op, false, sizeof(false));
+}
+
+void dfs(int number, int touch, int flag) {
+	if (number < 0 || number > 999 || touch+flag> M || answer < touch+flag) {
+		return;
+	}
+	if (number == W) {
+		answer = touch + flag;
+		return;
+	}
+
+	if (dp[number] == 0 || dp[number] > touch + flag) {
+		dp[number] = touch + flag;
+	}
+	
+	for (int i = 0; i < 1000; i++) {
+
+		if (i == 5) cout << dp[number] << endl;
+			for (int j = 1; j < 5; j++) {
+				if (op[j] == true) {
+					int temp = -1;
+					if (j == 1) temp = number + i;
+					else if (j == 2) temp = number - i;
+					else if (j == 3) temp = number * i;
+					else if (j == 4 && i != 0) temp = number / i;
+					if (temp!=-1)
+						dfs(number, dp[number]+dp[i],1);
+				}
+			}
+		}
+	
 }
 
 void init_dp(int number, int digit) {
 	if (number > 999) return;
 	if (digit == -1) {
 		for (int i = 0; i < N; i++) {
-			dp[num[i]] = 1;
+			origin[num[i]] = dp[num[i]] = 1;
 			init_dp(num[i], 0);
 		}
 		return;
 	}
-
-	if (dp[number] == 0) dp[number] = digit;
+	if (dp[number] == 0) origin[number] = dp[number] = digit;
 	else if(dp[number] != 0 && digit>0)
 		return;
 	
@@ -40,6 +70,7 @@ int main() {
 
 	freopen("old_phone.txt", "r", stdin);
 	int T;
+	int op_idx;
 	cin >> T;
 
 	for (int tc = 1; tc <= 1; tc++) {
@@ -49,20 +80,29 @@ int main() {
 			cin >> num[i];		
 		}
 		for (int i = 0; i < O; i++) {
-			op[i] = true;
+			cin >> op_idx;
+			op[op_idx] = true;
 		}	
 		
 		cin >> W;
+		cout << W << endl;
 		init_dp(0, -1);
-		if (dp[W] != 0) {
+		if (dp[W] > 0) {
 			answer = dp[W];
 		}
 		else {
-			continue;
+			for (int i = 0; i < 1000; i++) {
+				if (dp[i] && origin[i])
+					dfs(i, dp[i],0);
+			}		
+			answer = dp[W];
 		}
+		
 		for (int i = 0; i < 1000; i++) {
 			cout << i << " : " << dp[i] << endl;
 		}
+		
+		
 		cout << "#" << tc << " " << answer << endl;
 	}
 	return 0;
