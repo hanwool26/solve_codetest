d55d607c74b30ed507f6c57b8c540180bd9f54fc
Hanwool Lee <hanwool26@gmail.com>
2022-05-18 23:00:30 +0900

    Update 오래된_휴대폰.cpp

--- SWExpertAcademy/오래된_휴대폰.cpp
+++ SWExpertAcademy/오래된_휴대폰.cpp
@@ -12,11 +12,13 @@ int origin[1000];
 void init_test() {
 	answer = 21;
 	memset(dp, 0, sizeof(dp));
-	memset(op, false, sizeof(false));
+	memset(op, false, sizeof(op));
+	memset(origin, 0, sizeof(origin));
 }
 
 void dfs(int number, int touch, int flag) {
-	if (number < 0 || number > 999 || touch+flag> M || answer < touch+flag) {
+
+	if (touch + flag> M || touch+flag > answer) {
 		return;
 	}
 	if (number == W) {
@@ -24,41 +26,41 @@ void dfs(int number, int touch, int flag) {
 		return;
 	}
 
-	if (dp[number] == 0 || dp[number] > touch + flag) {
-		dp[number] = touch + flag;
-	}
-	
 	for (int i = 0; i < 1000; i++) {
+		if (!dp[i] || !origin[i]) continue;
 
-		if (i == 5) cout << dp[number] << endl;
-			for (int j = 1; j < 5; j++) {
-				if (op[j] == true) {
-					int temp = -1;
-					if (j == 1) temp = number + i;
-					else if (j == 2) temp = number - i;
-					else if (j == 3) temp = number * i;
-					else if (j == 4 && i != 0) temp = number / i;
-					if (temp!=-1)
-						dfs(number, dp[number]+dp[i],1);
-				}
+		for (int j = 1; j < 5; j++) {
+			if (op[j] == true) {
+				if (i == 0 && j == 4) continue;
+				int temp;
+				if (j == 1) temp = number + i;
+				else if (j == 2) temp = number - i;
+				else if (j == 3) temp = number * i;
+				else if (j == 4 && i != 0) temp = number / i;
+				if (temp < 0 || temp > 999) continue;
+				if (dp[temp] ==0 || dp[temp] > touch + dp[i] + 1){
+					dp[temp] = touch + dp[i] + 1;
+					dfs(temp, dp[temp], 1);
+				}				
 			}
 		}
-	
+	}
 }
 
 void init_dp(int number, int digit) {
-	if (number > 999) return;
+	if (number > 999 || digit > 3) return;
 	if (digit == -1) {
 		for (int i = 0; i < N; i++) {
 			origin[num[i]] = dp[num[i]] = 1;
-			init_dp(num[i], 0);
+			init_dp(num[i], 1);
 		}
 		return;
 	}
 	if (dp[number] == 0) origin[number] = dp[number] = digit;
-	else if(dp[number] != 0 && digit>0)
-		return;
-	
+	else if (dp[number] > digit) origin[number] = dp[number] = digit;
+
+
+
 	for (int i = 0; i < N; i++) {
 		init_dp(number * 10 + num[i], digit + 1);
 	}
@@ -73,19 +75,18 @@ int main() {
 	int op_idx;
 	cin >> T;
 
-	for (int tc = 1; tc <= 1; tc++) {
+	for (int tc = 1; tc <= T; tc++) {
 		init_test();
 		cin >> N >> O >> M;
 		for (int i = 0; i < N; i++) {
-			cin >> num[i];		
+			cin >> num[i];
 		}
 		for (int i = 0; i < O; i++) {
 			cin >> op_idx;
 			op[op_idx] = true;
-		}	
-		
+		}
+
 		cin >> W;
-		cout << W << endl;
 		init_dp(0, -1);
 		if (dp[W] > 0) {
 			answer = dp[W];
@@ -93,17 +94,11 @@ int main() {
 		else {
 			for (int i = 0; i < 1000; i++) {
 				if (dp[i] && origin[i])
-					dfs(i, dp[i],0);
-			}		
-			answer = dp[W];
-		}
-		
-		for (int i = 0; i < 1000; i++) {
-			cout << i << " : " << dp[i] << endl;
-		}
-		
-		
-		cout << "#" << tc << " " << answer << endl;
+					dfs(i, dp[i], 0);
+			}
+		}	
+
+		cout << "#" << tc << " " << (answer==21 ? -1 : answer) << endl;
 	}
 	return 0;
 }
