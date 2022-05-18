8cfdfdff0e988e08cf3c3c5eccf3c0d9f9062f6f
Hanwool Lee <hanwool26@gmail.com>
2022-05-18 15:48:38 +0900

    Update 오래된_휴대폰.cpp

--- SWExpertAcademy/오래된_휴대폰.cpp
+++ SWExpertAcademy/오래된_휴대폰.cpp
@@ -15,6 +15,25 @@ void init_test() {
 	memset(op, false, sizeof(false));
 }
 
+void init_dp(int number, int digit) {
+	if (number > 999) return;
+	if (digit == -1) {
+		for (int i = 0; i < N; i++) {
+			dp[num[i]] = 1;
+			init_dp(num[i], 0);
+		}
+		return;
+	}
+
+	if (dp[number] == 0) dp[number] = digit;
+	else if(dp[number] != 0 && digit>0)
+		return;
+	
+	for (int i = 0; i < N; i++) {
+		init_dp(number * 10 + num[i], digit + 1);
+	}
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
@@ -31,8 +50,20 @@ int main() {
 		}
 		for (int i = 0; i < O; i++) {
 			op[i] = true;
-		}			
+		}	
+		
 		cin >> W;
+		init_dp(0, -1);
+		if (dp[W] != 0) {
+			answer = dp[W];
+		}
+		else {
+			continue;
+		}
+		for (int i = 0; i < 1000; i++) {
+			cout << i << " : " << dp[i] << endl;
+		}
+		cout << "#" << tc << " " << answer << endl;
 	}
 	return 0;
 }
