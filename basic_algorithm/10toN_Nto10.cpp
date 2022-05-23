247a7c1013f15ae6591de33dc809ac460905e8b8
Hanwool Lee <hanwool26@gmail.com>
2022-05-23 11:03:06 +0900

    Update 10toN_Nto10.cpp

--- basic_algorithm/10toN_Nto10.cpp
+++ basic_algorithm/10toN_Nto10.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 string decimal_to_n(int num, int n) {
@@ -7,13 +6,11 @@ string decimal_to_n(int num, int n) {
 					   '8','9','A','B','C','D','E','F' };
 	int remain;
 	string str;
-	while (num / n > 0) {
+	while (num > 0) {
 		remain = num % n;
 		num /= n;
 		str = intToChar[remain] + str;
 	}
-	remain = num % n;
-	str = intToChar[remain] + str;
 	return str;
 }
 
@@ -21,14 +18,16 @@ int n_to_decimal(string num_str, int n) {
 	int value = 0;
 	int k;
 	for (int i = 0; i < num_str.length(); i++) {
-		int k = num_str[i] - '0';
+		if (n > 10 && num_str[i] >= 'A') k = num_str[i] - 'A' + 10;
+		k = num_str[i] - '0';
 		value = value * n + k;		
 	}
 	return value;
 }
 
-int main() {	
-	int num = 153;
+int main() {
+	
+	int num = 255;
 	int n = 16;
 	string str = decimal_to_n(num, n);
 	cout << str << endl;
