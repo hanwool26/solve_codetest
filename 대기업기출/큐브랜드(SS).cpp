e7772a487f1abba4fc3ce15573063843b4a06694
Hanwool Lee <hanwool26@gmail.com>
2022-05-23 16:33:57 +0900

    Update 큐브랜드(SS).cpp

--- 대기업기출/큐브랜드(SS).cpp
+++ 대기업기출/큐브랜드(SS).cpp
@@ -7,17 +7,24 @@
 32 11 49 11 30
 40 11 11 11 29
 40 42 50 29 28
-
-1. 인접큐브간 높이 차가 제일 큰 수 
--> 39 (50 - 11) 
-2. 인접큐브가 높이가 같을 때, 가장 큰 면적 ( 면적이 같을 경우, 더 큰 높이 숫자로 출력 ) 
+1. 인접큐브간 높이 차가 제일 큰 수
+-> 39 (50 - 11)
+2. 인접큐브가 높이가 같을 때, 가장 큰 면적 ( 면적이 같을 경우, 더 큰 높이 숫자로 출력 )
 -> ans : 11 8
-3. 빗물이 고여 웅덩이가 되었을 때, 가장 큰 면적 ( 면적이 같을 경우, 더 큰 높이 숫자로 출력 ) 
+3. 빗물이 고여 웅덩이가 되었을 때, 가장 큰 면적 ( 면적이 같을 경우, 더 큰 높이 숫자로 출력 )
 -> ans : 29 10
-
-제약사항 
+제약사항
 N <= 64, H <= 255
+
+how to solve No.3
+- BFS
+-> 자기 자신과 같거나 작으면 vector 좌표저장
+-> 자기 자신보다 작은수가 가장자리로 가면 return
+-> 자기 주변에 있는 큰 수중에 최소값을 저장 (해당 높이까지 물이 고임을 가정) 
+----> 일단 시간초과 나올 듯. 
 */
+
+
 
 
 #include <iostream>
@@ -25,10 +32,12 @@ N <= 64, H <= 255
 #include <vector>
 #include <cstring>
 #include <queue>
+#include <time.h>
 using namespace std;
 
 int N;
 int map[65][65];
+int c_map[65][65];
 bool visit[65][65];
 
 #define x first
@@ -36,28 +45,28 @@ bool visit[65][65];
 int dir_x[] = { -1, 1, 0, 0 };
 int dir_y[] = { 0, 0, -1, 1 };
 
-void print_map(){
-		for (int i = 0; i < N; i++){
-			for (int j = 0; j < N; j++){
-				cout << map[i][j] << " ";
-			}
-			cout << endl;
+void print_map() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << map[i][j] << " ";
 		}
+		cout << endl;
+	}
 }
 
-int get_max_space(int base, int x, int y){
+int get_max_space(int (*map)[65], int base, int x, int y) {
 	queue<pair<int, int>> q;
 	int nx, ny;
 	int count = 0;
 	q.push({ x, y });
 	visit[x][y] = true;
 
-	while (!q.empty()){
+	while (!q.empty()) {
 		x = q.front().x;
 		y = q.front().y;
 		q.pop();
 		count++;
-		for (int i = 0; i < 4; i++){
+		for (int i = 0; i < 4; i++) {
 			nx = x + dir_x[i];
 			ny = y + dir_y[i];
 			if (nx < 0 || nx > N - 1 || ny < 0 || ny > N - 1 || visit[nx][ny]) continue;
@@ -69,7 +78,7 @@ int get_max_space(int base, int x, int y){
 	return count;
 }
 
-void get_max_space_main(){
+void get_max_space_main() {
 	int max_cnt = 0;
 	int max_base = 0;
 	int cnt;
@@ -77,16 +86,16 @@ void get_max_space_main(){
 	memset(visit, false, sizeof(visit));
 
 
-	for (int i = 0; i < N; i++){
-		for (int j = 0; j < N; j++){
-			if (visit[i][j] != true){
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (visit[i][j] != true) {
 				base = map[i][j];
-				cnt = get_max_space(base, i, j);
-				if (max_cnt < cnt){
+				cnt = get_max_space(map, base, i, j);
+				if (max_cnt < cnt) {
 					max_cnt = cnt;
 					max_base = base;
 				}
-				else if (max_cnt == cnt){
+				else if (max_cnt == cnt) {
 					max_base = max(max_base, base);
 				}
 			}
@@ -95,70 +104,123 @@ void get_max_space_main(){
 	cout << max_base << " " << max_cnt << " ";
 }
 
-int bfs(int base, int x, int y){
-	memset(visit, false, sizeof(visit));
+vector<pair<int, int>> v;
+
+int bfs(int base, int x, int y) {
+	int visit[65][65];
+	//
+	
 	//if (x == 0 || y == 0 || x == N - 1 || y == N - 1) return 0;
+	v.clear();
+	
 	int nx, ny;
 	int cnt = 0;
+	int min_h = 256;
 	queue<pair<int, int>> q;
 	q.push({ x, y });
 	visit[x][y] = true;
-	while (!q.empty()){
+	while (!q.empty()) {
 		x = q.front().x;
 		y = q.front().y;
 		q.pop();
-		cnt++;
-		for (int i = 0; i < 4; i++){
+		v.push_back({ x,y });
+		for (int i = 0; i < 4; i++) {
 			nx = x + dir_x[i];
 			ny = y + dir_y[i];
-			if (nx < 0 || nx > N - 1 || ny < 0 || ny > N - 1) continue;
+			if (nx <0 || nx > N - 1 || ny < 0 || ny > N - 1) continue;
+			if (map[nx][ny] > base) {
+				min_h = min(min_h, map[nx][ny]);
+			}
+
 			if (base < map[nx][ny] || visit[nx][ny]) continue;
-			if ((nx == 0 || nx == N - 1 || ny == 0 || ny == N - 1) && map[nx][ny] != base) continue;
+			if ((nx == 0 || nx == N - 1 || ny == 0 || ny == N - 1) && map[nx][ny] < base) return 0;
+
 			q.push({ nx, ny });
 			visit[nx][ny] = true;
 		}
 	}
-	return cnt;
+	return min_h;
 }
 
-void bfs_main(){
+void copy_map() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			c_map[i][j] = map[i][j];
+		}
+	}
+}
+
+
+void bfs_main() {
 	int max_cnt = 0;
 	int max_base = 0;
-	int cnt;
+	int min_h;
+	int cnt = 0;
 	int base;
-	for (int i = 0; i < N; i++){
-		for (int j = 0; j < N; j++){
+	memset(visit, false, sizeof(visit));
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			base = map[i][j];
-			cnt = bfs(base, i, j);
-			if (max_cnt < cnt){
-				max_cnt = cnt;
-				max_base = base;
-			}
-			else if (max_cnt == cnt){
-				max_base = max(max_base, base);
+			min_h = bfs(base, i, j);
+			if (min_h != 0) {
+				copy_map();
+				for (auto& k : v) {
+					c_map[k.x][k.y] = min_h;
+				}
+				
+				cnt = get_max_space(c_map, min_h, i, j);
+
+				if (max_cnt < cnt) {
+					max_cnt = cnt;
+					max_base = min_h;
+				}
+				else if (max_cnt == cnt) {
+					max_base = max(max_base, min_h);
+				}
 			}
 		}
 	}
 	cout << max_base << " " << max_cnt << endl;
 }
 
+void make_rand() {
+	int n = 0;
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 20; j++) {
+			
+			do {
+				n = rand() % 255;
+				if (n == 0) continue;
+				cout << n << " ";
 
-int main(){
+			} while (n == 0);
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+
+	clock_t start, end;
+	start = clock();
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
 	freopen("큐브랜드.txt", "r", stdin);
 	int T; cin >> T;
 
-	for (int tc = 1; tc <= T; tc++){
+	for (int tc = 1; tc <= T; tc++) {
 		cin >> N;
-		for (int i = 0; i < N; i++){
-			for (int j = 0; j < N; j++){
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
 				cin >> map[i][j];
 			}
 		}
+		//make_rand();
 		get_max_space_main();
 		bfs_main();
 	}
+	end = clock();
+	cout << (double)(end - start) / CLOCKS_PER_SEC << endl;
 	return 0;
 }
