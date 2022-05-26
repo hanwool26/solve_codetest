965e057283766949ac0a693ea46be86ec5e1cb62
Hanwool Lee <hanwool26@gmail.com>
2022-05-26 23:17:27 +0900

    Update 멀티태스킹.cpp

--- 인프런코테강의/멀티태스킹.cpp
+++ 인프런코테강의/멀티태스킹.cpp
@@ -1,26 +1,55 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
-vector<int> v;
+typedef struct time{
+	int idx;
+	int t;
+	time(int idx, int t){
+		this->idx = idx;
+		this->t = t;
+	}
+	bool operator<(struct time &b){
+		return this->t < b.t;
+	}
+}Time;
+
 int N, K;
-int main() {
-	int temp;
-	int total_t = 0; // total time
-	int cycle;
-	int min_task = 1e9;
+vector<Time> v;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
 	freopen("멀티태스킹.txt", "r", stdin);
 	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> temp;
-		total_t += temp;
-		v.push_back(temp);
-		min_task = min(min_task, temp);
+	int t;
+	for (int i = 1; i <= N; i++){
+		cin >> t;
+		v.push_back(Time(i, t));
 	}
-
 	cin >> K;
-	if(K>N)	cycle = K / N; // 정전 타임이 작업 수보다 크다면
-	for(int i =0;)
 
+	sort(v.begin(), v.end());
+	int total_time = 0;
+	int idx = -1;
+	int per_time = 0;
+	for (int i = 0; i < v.size(); i++){
+		per_time = v[i].t;
+		for (int j = i; j < v.size(); j++){			
+			v[j].t = v[j].t - per_time;
+			total_time += per_time; 
+			if (total_time >= K) {
+				idx = j;
+				break;
+			}
+		}
+		if (idx != -1) break;
+	}
+	if (v[idx].t == 0) cout << -1 << endl;
+	else cout << v[idx].idx << endl;
 
+	return 0;
 }
