43f683d1a38d9268a7d0c186f46c00d854218794
Hanwool Lee <hanwool26@gmail.com>
2022-05-26 09:20:08 +0900

    Update 아나그램.cpp

--- 인프런코테강의/아나그램.cpp
+++ 인프런코테강의/아나그램.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 using namespace std;
 string S, T;
-int encode(string str) {
-	int sum = 0;
 
-	for (int i = 0; i< str.length(); i++) {
-		int k = str[i] - 'a';
-		sum += pow((k + 26), 2);
-	}
-	return sum;
-}
 int main() {	
 	freopen("아나그램.txt", "r", stdin);
 	cin >> S >> T;
-	int code = encode(T);
 	string sub_str;
 	int cnt = 0;
 
-	for (int i = 0; i < (S.length()-T.length()) + 1; i++) {
-		sub_str = S.substr(i, T.length());
-		if (code == encode(sub_str)) cnt++;
+	unordered_map<char, int> tm;
+	unordered_map<char, int> sm;
+	
+	// init map(hash table)
+	for (int i = 0; i < T.length(); i++) {
+		sm[S[i]]++;
+		tm[T[i]]++;
+	}
+	int lt, rt;
+	lt = 0;
+	rt = T.length() - 1;
+	while (rt < S.length()) {
+		if (sm == tm) cnt++;
+		sm[S[lt]]--;		
+		if (sm[S[lt]] == 0) sm.erase(S[lt]);
+		sm[S[++rt]]++;
+		lt++;
 	}
 	cout << cnt << endl;
 }
