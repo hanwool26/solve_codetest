68a90fa2c6caccb5662bc8d7f3116462108988b8
Hanwool Lee <hanwool26@gmail.com>
2022-05-27 22:09:05 +0900

    Update 스도쿠게임.cpp

--- 인프런코테강의/스도쿠게임.cpp
+++ 인프런코테강의/스도쿠게임.cpp
@@ -25,12 +25,24 @@ bool check_sdoku(int idx) {
 
 	for (int i = 0; i < 9; i++) {
 		if ((s.find(map[x][i]) != s.end()) && map[x][i] != 0) return false;
-		if(map[x][i]!=0) s.insert(map[x][i]);
+		if (map[x][i] != 0) s.insert(map[x][i]);
 	}
 	s.clear();
 	for (int i = 0; i < 9; i++) {
 		if ((s.find(map[i][y]) != s.end()) && map[i][y] != 0) return false;
 		if (map[i][y] != 0) s.insert(map[i][y]);
+	}
+
+	s.clear();
+	
+	int gx = ((x / 3) * 3);
+	int gy = ((y / 3) * 3);
+	//cout << gx << " " << gy << endl; 
+	for (int i = gx; i < gx + 3; i++){
+		for (int j = gy; j < gy + 3; j++){
+			if ((s.find(map[i][j]) != s.end()) && map[i][j] != 0) return false;
+			if (map[i][j] != 0) s.insert(map[i][j]);
+		}
 	}
 	return true;
 }
@@ -44,10 +56,10 @@ void dfs(int idx) {
 		print_map();
 		return;
 	}
-	
+
 	int cx = v[idx].first;
 	int cy = v[idx].second;
-	for (int k = 1; k <=9; k++) {
+	for (int k = 1; k <= 9; k++) {
 		map[cx][cy] = k;
 		if (!check_sdoku(idx)) {
 			map[cx][cy] = 0;	// 0으로 바꿔줘야 부모노드에서 sdoku 체크할 때 중복 숫자로인한 잘못된 return이 없음
@@ -62,7 +74,7 @@ int main() {
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
 			cin >> map[i][j];
-			if (map[i][j] == 0) v.push_back({ i,j });
+			if (map[i][j] == 0) v.push_back({ i, j });
 		}
 	}
 	hole = v.size();
