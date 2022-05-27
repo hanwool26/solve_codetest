54afb93a685e5c1a92d4cb2bab0557f3571daab8
Hanwool Lee <hanwool26@gmail.com>
2022-05-27 19:32:03 +0900

    Update 톰과제리.cpp

--- 인프런코테강의/톰과제리.cpp
+++ 인프런코테강의/톰과제리.cpp
@@ -8,7 +8,7 @@ int N;
 pair<int, int> start;
 pair<int, int> finish;
 int map[100][100];
-bool visit[100][100];
+int visit[100][100];
 
 enum {
 	UP,
@@ -17,8 +17,8 @@ enum {
 	RIGHT
 };
 
-int dir_x[] = { 1,-1,0,0 };
-int dir_y[] = { 0,0,-1,1 };
+int dir_x[] = { -1, 1, 0, 0 };
+int dir_y[] = { 0, 0, -1, 1 };
 
 typedef struct node {
 	int x;
@@ -39,34 +39,34 @@ int answer = 1e9;
 void bfs(int x, int y) {
 	queue<Node> q;
 	q.push(Node(x, y, -1, 0));
-	visit[x][y] = true;
-	
+	visit[x][y] = 0;
+
 	while (!q.empty()) {
 		int x = q.front().x;
 		int y = q.front().y;
 		int dir = q.front().dir;
 		int dir_cnt = q.front().dir_cnt;
+	
 		q.pop();
+		if (x == finish.first && y == finish.second) {
+			answer = min(answer, dir_cnt);
+		}
 
 		for (int i = 0; i < 4; i++) {
 			int nx = x + dir_x[i];
 			int ny = y + dir_y[i];
 
-			if (nx < 1 || nx > N  || ny < 1 || ny > N || visit[nx][ny] || 
-				map[nx][ny] == 1 )continue;
-			
+			if (nx < 1 || nx > N || ny < 1 || ny > N || ((i != dir) && (visit[nx][ny] < dir_cnt + 1)) || ((i == dir) && (visit[nx][ny] < dir_cnt)) ||
+				map[nx][ny] == 1) continue;
+
 			if (dir != i) { // 방향이 다르면
 				q.push(Node(nx, ny, i, dir_cnt + 1));
+				visit[nx][ny] = dir_cnt+1;
 			}
 			else { // 방향이 같으면
 				q.push(Node(nx, ny, i, dir_cnt));
-			}
-			if (nx == finish.first && ny == finish.second) {
-				answer = min(answer, dir_cnt);
-			}
-			else {
-				visit[nx][ny] = true;
-			}
+				visit[nx][ny] = dir_cnt;
+			}			
 		}
 	}
 }
@@ -82,12 +82,15 @@ int main() {
 	}
 	int x, y;
 	cin >> x >> y;
-	start = { x,y };
+	start = { x, y };
 	cin >> x >> y;
-	finish = { x, y };	
-
+	finish = { x, y };
+	for (int i = 1; i <= N; i++){
+		for (int j = 1; j <= N; j++){
+			visit[i][j] = 1e9;
+		}
+	}
 	bfs(start.first, start.second);
-	cout << answer << endl;
+	cout << ((answer==1e9)?-1:answer-1) << endl;
 	return 0;
-
 }
