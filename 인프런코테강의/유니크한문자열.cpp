c34395aca88cb3a1fbf240a45073612a29368833
Hanwool Lee <hanwool26@gmail.com>
2022-05-30 16:49:20 +0900

    Update 유니크한문자열.cpp

--- 인프런코테강의/유니크한문자열.cpp
+++ 인프런코테강의/유니크한문자열.cpp
@@ -1,40 +1,26 @@
 #include <iostream>
-#include <unordered_set>
 #include <string>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 int N;
 char uni;
 
-
 int main() {
 	freopen("유니크한문자열.txt", "r", stdin);
 	cin >> N >> uni;
 	uni = toupper(uni);
-	string temp;
-	char upper;
-
-	vector<char> v;
 	for (int i = 0; i < N; i++) {
-		cin >> temp;
-		//unordered_set<char> s;
-		bool flag = false;
-		for (int i = 0; i < temp.length(); i++) {
-			upper = toupper(temp[i]);	
-			int j;
-			for ( j = 0; j < v.size(); j++) {
-				if (v[j] == upper) break;
+		string a;
+		cin >> a;
+		transform(a.begin(), a.end(), a.begin(), ::toupper); // 모두 대문자로 
+		if (a.find(uni) != string::npos) { // 문자가 존재 
+			string res = "";
+			for (int i = 0; i < a.size(); i++) {
+				if (a.find(a[i]) == i) res += a[i];	// 문자열이 처음 등장. (이 경우 처음 등장하는 문자의 인덱스와 일치함)
 			}
-			if (upper == uni) flag = true;
-			if(j==v.size()) v.push_back(upper);
-		}	
-		if (flag) {
-			for (char k : v) cout << k;
-			cout << endl;		
+			cout << res << endl;
 		}
-		v.clear();		
 	}
 	return 0;
 }
