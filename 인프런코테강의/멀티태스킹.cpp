e5df9d4fa851787aa400db50f0e42a10d31c65c7
Hanwool Lee <hanwool26@gmail.com>
2022-05-30 16:50:09 +0900

    Update 멀티태스킹.cpp

--- 인프런코테강의/멀티태스킹.cpp
+++ 인프런코테강의/멀티태스킹.cpp
@@ -1,55 +1,69 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-typedef struct time{
-	int idx;
-	int t;
-	time(int idx, int t){
+typedef struct task {
+	long long idx;
+	long long time;
+	bool finish;
+	task(long long idx, long long time, bool finish) {
 		this->idx = idx;
-		this->t = t;
+		this->time = time;
+		this->finish = finish;
 	}
-	bool operator<(struct time &b){
-		return this->t < b.t;
+	bool operator<(struct task& t) {
+		return this->time < t.time;
 	}
-}Time;
+}Task;
 
-int N, K;
-vector<Time> v;
+bool ascending_idx(Task a, Task b) {
+	return a.idx < b.idx;
+}
+vector<Task> v;
 
-int main(){
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+long long N, K;
+int main() {
+	long long temp;
 
 	freopen("멀티태스킹.txt", "r", stdin);
 	cin >> N;
-	int t;
-	for (int i = 1; i <= N; i++){
-		cin >> t;
-		v.push_back(Time(i, t));
+	v.push_back(Task(0, 0, true));
+	for (int i = 1; i <= N; i++) {
+		cin >> temp;
+		v.push_back(Task(i, temp, false));
 	}
 	cin >> K;
 
 	sort(v.begin(), v.end());
-	int total_time = 0;
-	int idx = -1;
-	int per_time = 0;
-	for (int i = 0; i < v.size(); i++){
-		per_time = v[i].t;
-		for (int j = i; j < v.size(); j++){			
-			v[j].t = v[j].t - per_time;
-			total_time += per_time; 
-			if (total_time >= K) {
-				idx = j;
+
+	long long res = N; // 남아있는 작업. 
+	long long per_time;
+	long long total_time = 0;
+	long long remain_t = 0;
+	for (int i = 1; i < v.size(); i++) {
+		per_time = (v[i].time - v[i-1].time) * res;		
+		if (total_time + per_time > K) {
+			remain_t = K - total_time;
+			break;
+		}
+		total_time += per_time;
+		res--;
+		v[i].finish = true;
+	}
+	long long answer = -1;
+	if (res!=0)
+		remain_t %= res;
+		sort(v.begin(), v.end(), ascending_idx);
+
+		for (auto& i : v) {
+			if (i.finish == true) continue;
+			if (remain_t == 0) {
+				answer = i.idx;
 				break;
 			}
-		}
-		if (idx != -1) break;
-	}
-	if (v[idx].t == 0) cout << -1 << endl;
-	else cout << v[idx].idx << endl;
-
+			remain_t--;
+		}	
+	cout << answer << endl;
 	return 0;
 }
